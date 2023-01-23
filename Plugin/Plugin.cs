e4d5e131504b6478b7642abf00fe539abdc85e50
whateverusername0 <whateveremail>
2023-01-24 00:07:41 +1000

    Add project files.

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BepInEx;
+
+namespace Plugin
+{
+    [BepInPlugin("com.bepinex.plugin.vrmod", "Ultrakill VR UPDATE!!!", "0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+    }
+}
