e591c18b303202a4d80acf8f876bc832142ba677
whateverusername0 <whateveremail>
2023-04-16 02:35:22 +1000

    player scale fix (hopefully)

---
+++ VRTRAKILL/VRTRAKILL/Movement/Patches/VRPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+namespace Plugin.VRTRAKILL.Movement.Patches
+{
+    [HarmonyPatch] internal class VRPlayer
+    {
+        // for some reason when in VR the player is the double of it's size so i need to fix that
+        // placeholder
+        static float PlayerScaleMultiplier = 0.75f;
+
+        [HarmonyPrefix] [HarmonyPatch(typeof(NewMovement), "Start")] static void ShirnkPlayer(NewMovement __instance)
+        {
+            __instance.gameObject.transform.localScale *= PlayerScaleMultiplier;
+        }
+    }
+}
