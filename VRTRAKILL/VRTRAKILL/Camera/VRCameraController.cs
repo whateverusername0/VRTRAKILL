a4beeaa6d06a8c168715493b0f2d9d12d5f64cfe
whateverusername0 <whateveremail>
2023-04-17 18:08:31 +1000

    separated camera from patches

---
+++ VRTRAKILL/VRTRAKILL/Camera/VRCameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRCamera
+{
+    internal class VRCameraController : MonoBehaviour
+    {
+        // Camera getting posessed fix (also CLEAAAAAAAAN)
+        private void Start()
+        {
+            NewMovement.Instance.transform.right = this.transform.right;
+            NewMovement.Instance.transform.forward = this.transform.forward;
+        }
+
+        private void Update()
+        {
+            // Smooth turn
+            if (NewMovement.Instance.dead) return;
+            NewMovement.Instance.gameObject.transform.rotation =
+                Quaternion.Euler(NewMovement.Instance.transform.rotation.eulerAngles.x,
+                                 Vars.MainCamera.transform.rotation.eulerAngles.y,
+                                 NewMovement.Instance.transform.rotation.eulerAngles.z);
+
+            Vars.VRCameraContainer.transform.rotation = Quaternion.Euler(0f, Input.VRInputVars.TurnOffset, 0f);
+        }
+    }
+}
