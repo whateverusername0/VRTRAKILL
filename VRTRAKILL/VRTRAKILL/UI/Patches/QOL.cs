d6fd315d2d483615e206babc5d317f7dd41729c5
whateverusername0 <whateveremail>
2023-04-17 15:01:39 +1000

    refactor

--- VRTRAKILL/VRTRAKILL/UI/Patches/QOL.cs
+++ VRTRAKILL/VRTRAKILL/UI/Patches/QOL.cs
@@ -3,15 +3,15 @@ using HarmonyLib;
 
 namespace Plugin.VRTRAKILL.UI.Patches
 {
-    // no more ui eye sore
     [HarmonyPatch] internal class QOL
     {
-        // removes black color on death screen, hurt red color & parry flash
         [HarmonyPrefix] [HarmonyPatch(typeof(CanvasController), "Awake")] static void RemoveEyeSore(CanvasController __instance)
         {
-            __instance.gameObject.transform.Find("HurtScreen").GetComponent<Image>().enabled = false;
-            __instance.gameObject.transform.Find("BlackScreen").GetComponent<Image>().enabled = false;
-            __instance.gameObject.transform.Find("ParryFlash").GetComponent<Image>().enabled = false;
+            string[] EyeSore =
+            {
+                "HurtScreen", "BlackScreen", "ParryFlash"
+            };
+            foreach (string Sore in EyeSore) __instance.gameObject.transform.Find(Sore).GetComponent<Image>().enabled = false;
         }
     }
 }
