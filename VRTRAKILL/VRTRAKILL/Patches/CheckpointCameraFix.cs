2705288f035546ef28d2bae555e3621fc8e04423
whateverusername0 <whateveremail>
2023-04-17 15:01:10 +1000

    fix camera getting possessed on checkpoint

---
+++ VRTRAKILL/VRTRAKILL/Patches/CheckpointCameraFix.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.Patches
+{
+    [HarmonyPatch] internal class CheckpointCameraFix
+    {
+        [HarmonyPrefix] [HarmonyPatch(typeof(NewMovement), "DeactivatePlayer")] static bool DontDisableCCOnDeath
+        (NewMovement __instance)
+        {
+            __instance.activated = false;
+            // MonoSingleton<CameraController>.Instance.activated = true; // this should work (i wish)
+            MonoSingleton<GunControl>.Instance.NoWeapon();
+            MonoSingleton<FistControl>.Instance.NoFist();
+            if (__instance.sliding) __instance.StopSlide();
+            return false;
+        }
+    }
+}
