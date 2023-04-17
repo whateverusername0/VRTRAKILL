2d70c7cf5049928f41327005a2459f4854917dde
whateverusername0 <whateveremail>
2023-04-17 15:01:31 +1000

    formatting

--- VRTRAKILL/VRTRAKILL/Patches/Helpers.cs
+++ VRTRAKILL/VRTRAKILL/Patches/Helpers.cs
@@ -6,6 +6,7 @@ namespace Plugin.VRTRAKILL.Patches
     internal class Helpers
     {
         // ty huskvr you pretty
+        // might remove later because it's unnecessary
         [HarmonyPrefix] [HarmonyPatch(typeof(PostProcessV2_Handler), "SetupRTs")] static bool DontUpdateRTsIfCamNull(PostProcessV2_Handler __instance)
         {
             if (Traverse.Create(__instance).Field("mainCam").GetValue() != null
