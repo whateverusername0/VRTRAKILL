ee6ced98fe42f4848f0e4b45717dbf4d62423f1a
whateverusername0 <whateveremail>
2023-04-18 01:21:00 +1000

    preparations

---
+++ VRTRAKILL/VRTRAKILL/Input/NewInput/InputValues.cs
@@ -0,0 +1,26 @@
+﻿using Valve.VR;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.Input.NewInput
+{
+    internal class InputValues
+    {
+        // it doesn't matter if it seems reasonable or not, if it's there - it's useful
+
+        public static Vector2 MoveAxis()
+        { return SteamVR_Actions._default.Movement.GetAxis(SteamVR_Input_Sources.Any); }
+        public static float MoveXAxis()
+        { return SteamVR_Actions._default.Movement.GetAxis(SteamVR_Input_Sources.Any).x; }
+        public static float MoveYAxis()
+        { return SteamVR_Actions._default.Movement.GetAxis(SteamVR_Input_Sources.Any).y; }
+
+        public static Vector2 TurnAxis()
+        { return SteamVR_Actions._default.Turn.GetAxis(SteamVR_Input_Sources.Any); }
+        public static float TurnXAxis()
+        { return SteamVR_Actions._default.Turn.GetAxis(SteamVR_Input_Sources.Any).x; }
+        public static float TurnYAxis()
+        { return SteamVR_Actions._default.Turn.GetAxis(SteamVR_Input_Sources.Any).y; }
+
+
+    }
+}
