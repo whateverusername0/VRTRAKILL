1845412e40d6188b7efcb42d1dbba79b51f84883
whateverusername0 <whateveremail>
2023-04-18 18:16:40 +1000

    moved

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Patches/DebugArms.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Valve.VR;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms
+{
+    // note: remove this when time comes
+    [HarmonyPatch] internal class DebugArms
+    {
+        [HarmonyPrefix] [HarmonyPatch(typeof(CameraController), "Start")] static void AddPlaceholderHands(NewMovement __instance)
+        {
+            // Left Hand
+            GameObject LHGO = new GameObject("Left Controller"); LHGO.transform.parent = __instance.transform;
+            SteamVR_Behaviour_Pose LeftHand = LHGO.AddComponent<SteamVR_Behaviour_Pose>();
+            LeftHand.origin = __instance.gameObject.transform;
+            LeftHand.poseAction = SteamVR_Actions._default.LHP; LeftHand.inputSource = SteamVR_Input_Sources.LeftHand;
+            // Left Hand Model
+            GameObject LHMGO = new GameObject("Model"); LHMGO.transform.parent = LHGO.transform;
+            SteamVR_RenderModel LHMGORM =  LHMGO.AddComponent<SteamVR_RenderModel>(); // it should create model automatically
+            LHMGORM.createComponents = true;
+
+            // Right Hand
+            GameObject RHGO = new GameObject("Right Controller"); RHGO.transform.parent = __instance.transform;
+            SteamVR_Behaviour_Pose RightHand = RHGO.AddComponent<SteamVR_Behaviour_Pose>();
+            RightHand.origin = __instance.gameObject.transform;
+            RightHand.poseAction = SteamVR_Actions._default.RHP; RightHand.inputSource = SteamVR_Input_Sources.RightHand;
+            // Right Hand Model
+            GameObject RHMGO = new GameObject("Model"); RHMGO.transform.parent = RHGO.transform;
+            SteamVR_RenderModel RHMGORM = RHMGO.AddComponent<SteamVR_RenderModel>(); // it should create model automatically
+            RHMGORM.createComponents = true;
+        }
+    }
+}
---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/VRCameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.VRCamera
+{
+    internal class VRCameraController : MonoBehaviour
+    {
+        // Camera getting posessed fix (also CLEAAAAAAAAN)
+        private void Start()
+        {
+            NewMovement.Instance.gameObject.transform.eulerAngles = this.gameObject.transform.eulerAngles;
+            NewMovement.Instance.gameObject.transform.forward = this.gameObject.transform.forward;
+        }
+
+        private void Update()
+        {
+            // Smooth turn
+            if (NewMovement.Instance.dead) return;
+            NewMovement.Instance.gameObject.transform.rotation =
+                Quaternion.Euler(NewMovement.Instance.transform.rotation.eulerAngles.x,
+                                 Vars.MainCamera.transform.rotation.eulerAngles.y,
+                                 NewMovement.Instance.transform.rotation.eulerAngles.z);
+
+            Vars.VRCameraContainer.transform.rotation = Quaternion.Euler(0f, Input.VRInputVars.TurnOffset, 0f);
+        }
+    }
+}
