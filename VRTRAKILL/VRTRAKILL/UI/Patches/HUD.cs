b8710d5de2d11329b956d599370ea9555efbfbbe
whateverusername0 <whateveremail>
2023-04-18 20:02:15 +1000

    reformat

--- VRTRAKILL/VRTRAKILL/UI/Patches/HUD.cs
+++ VRTRAKILL/VRTRAKILL/UI/Patches/HUD.cs
@@ -5,18 +5,18 @@ namespace Plugin.VRTRAKILL.UI.Patches
 {
     [HarmonyPatch(typeof(HUDPos))] internal class HUD
     {
-        [HarmonyPrefix] [HarmonyPatch("Start")] static void FixPos(HUDPos __instance, ref Vector3 ___defaultPos)
+        [HarmonyPrefix] [HarmonyPatch("Start")] static void FixPos(HUDPos __instance)
         {
             // HuskVR you pretty
             if (__instance.gameObject.name == "GunCanvas")
             {
                 __instance.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-                ___defaultPos = new Vector3(-0.75f, -0.4f, 1.6f);
+                __instance.defaultPos = new Vector3(-0.75f, -0.4f, 1.6f);
             }
             if (__instance.gameObject.name == "StyleCanvas")
             {
                 __instance.transform.localScale = new Vector3(0.002f, 0.002f, 0.001f);
-                ___defaultPos = new Vector3(1.3f, 0.2f, 1.6f);
+                __instance.defaultPos = new Vector3(1.3f, 0.2f, 1.6f);
             }
         }
     }
