d3e417f1838924577c7f95dfe42be0c127fe6cd1
whateverusername0 <whateveremail>
2023-04-18 18:15:18 +1000

    new input preparations (might get removed, idk)

---
+++ VRTRAKILL/VRTRAKILL/Input/NewInput/VRInputManager.cs
@@ -0,0 +1,42 @@
+﻿using Plugin.Helpers;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.Input.NewInput
+{
+    [ConfigureSingleton(SingletonFlags.PersistAutoInstance)] public class VRInputManager : MonoSingleton<VRInputManager>
+    {
+        private static bool
+            WalkForward = false,  WalkForwardState = false,
+            WalkBackward = false, WalkBackwardState = false,
+            WalkLeft = false,     WalkLeftState = false,
+            WalkRight = false,    WalkRightState = false,
+
+            Jump = false,
+            Dash = false,
+            Slide = false;
+
+        private static bool
+            RHPrimaryFire = false,
+            RHAltFire = false,
+            ChangeWeaponVariation = false,
+            OpenWeaponWheel = false;
+
+        private static bool
+            Punch = false,
+            SwapHand = false,
+            Whiplash = false;
+
+        private static bool
+            Slot0 = false,
+            Slot1 = false, Slot2 = false, Slot3 = false,
+            Slot4 = false, Slot5 = false, Slot6 = false,
+            Slot7 = false, Slot8 = false, Slot9 = false;
+
+        private static bool Escape;
+
+        private void Update()
+        {
+
+        }
+    }
+}
