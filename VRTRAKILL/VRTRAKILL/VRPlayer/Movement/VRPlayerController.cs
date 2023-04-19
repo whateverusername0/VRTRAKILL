710f8fbf0c6f92d1e66595aa602f0060ed2f759a
whateverusername0 <whateveremail>
2023-04-19 20:50:28 +1000

    fix player height

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Movement/VRPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Movement
+{
+    internal class VRPlayerController : MonoBehaviour
+    {
+        public CapsuleCollider CC;
+
+        private void Start()
+        {
+            CC = GetComponent<CapsuleCollider>();
+        }
+
+        private void Update()
+        {
+            // Update player's height
+            float DistanceFromFloor = Vector3.Dot(Vars.VRCameraContainer.transform.localPosition, Vector3.up);
+            CC.height = Mathf.Max(CC.radius, DistanceFromFloor);
+
+            CC.center = Vars.VRCameraContainer.transform.localPosition - 0.5f * DistanceFromFloor * Vector3.up;
+        }
+    }
+}
