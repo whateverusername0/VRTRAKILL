d33e7d0bcb3b64d9e312d6fc89524ca7b8779a48
whateverusername0 <whateveremail>
2023-04-19 20:50:58 +1000

    unfixed "fix" of checkpoint clowning

--- VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/FixCameraOnRespawn.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/FixCameraOnRespawn.cs
@@ -1,15 +1,13 @@
 ﻿using HarmonyLib;
+using System;
 
 namespace Plugin.VRTRAKILL.VRPlayer.VRCamera.Patches
 {
     [HarmonyPatch(typeof(NewMovement))] internal class FixCameraOnRespawn
     {
-        [HarmonyPrefix] [HarmonyPatch(nameof(NewMovement.OnEnable))] static void SetPlayerRotation(NewMovement __instance)
+        [HarmonyPrefix] [HarmonyPatch(nameof(NewMovement.Respawn))] static void SetPlayerRotation(NewMovement __instance)
         {
-            NewMovement.Instance.gameObject.transform.eulerAngles = Vars.VRCameraContainer.transform.eulerAngles; // idk if it's necessary
-
-            NewMovement.Instance.gameObject.transform.right = Vars.VRCameraContainer.transform.right;
-            NewMovement.Instance.gameObject.transform.forward = Vars.VRCameraContainer.transform.forward;
+            // HOW :(
         }
     }
 }
