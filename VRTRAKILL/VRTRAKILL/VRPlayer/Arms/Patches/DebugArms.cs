2c896da9a93903c976f470bb8f3a3aeca6692776
whateverusername0 <whateveremail>
2023-04-20 00:33:18 +1000

    commented out - it's meant to be debug

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Patches/DebugArms.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Patches/DebugArms.cs
@@ -6,36 +6,32 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms.Patches
 {
     [HarmonyPatch(typeof(NewMovement))] internal class DebugArms
     {
-        [HarmonyPostfix] [HarmonyPatch(nameof(NewMovement.Start))] public static void AddDebugHands(NewMovement __instance)
+        /* [HarmonyPostfix] [HarmonyPatch(nameof(NewMovement.Start))] */ public static void AddDebugHands(NewMovement __instance)
         {
             __instance.gameObject.SetActive(false);
 
             // Left Hand
-            GameObject LHGO = new GameObject("Left Controller"); LHGO.transform.parent = Vars.VRCameraContainer.transform;
-
+            GameObject LHGO = new GameObject("Left Controller");
+            LHGO.transform.parent = Vars.VRCameraContainer.transform;
             SteamVR_Behaviour_Pose LeftHand = LHGO.AddComponent<SteamVR_Behaviour_Pose>();
             LeftHand.poseAction = SteamVR_Actions._default.LeftPose;
             LeftHand.inputSource = SteamVR_Input_Sources.LeftHand;
-
             // Left Hand Model
             GameObject LHMGO = new GameObject("Model"); LHMGO.transform.parent = LHGO.transform;
             LHMGO.AddComponent<LeftArmController>();
-
-            SteamVR_RenderModel LHMGORM = LHMGO.AddComponent<SteamVR_RenderModel>(); // it should create model automatically
+            SteamVR_RenderModel LHMGORM = LHMGO.AddComponent<SteamVR_RenderModel>();
             LHMGORM.createComponents = true;
 
             // Right Hand
-            GameObject RHGO = new GameObject("Right Controller"); RHGO.transform.parent = Vars.VRCameraContainer.transform;
-
+            GameObject RHGO = new GameObject("Right Controller");
+            RHGO.transform.parent = Vars.VRCameraContainer.transform;
             SteamVR_Behaviour_Pose RightHand = RHGO.AddComponent<SteamVR_Behaviour_Pose>();
             RightHand.poseAction = SteamVR_Actions._default.RightPose;
             RightHand.inputSource = SteamVR_Input_Sources.RightHand;
-
             // Right Hand Model
             GameObject RHMGO = new GameObject("Model"); RHMGO.transform.parent = RHGO.transform;
             RHMGO.AddComponent<RightArmController>();
-
-            SteamVR_RenderModel RHMGORM = RHMGO.AddComponent<SteamVR_RenderModel>(); // it should create model automatically
+            SteamVR_RenderModel RHMGORM = RHMGO.AddComponent<SteamVR_RenderModel>();
             RHMGORM.createComponents = true;
 
             __instance.gameObject.SetActive(true);
