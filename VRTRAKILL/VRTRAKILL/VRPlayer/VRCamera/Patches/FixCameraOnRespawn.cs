57834b25a335baa7ab9e04357400f66b1a0ff7fa
whateverusername0 <whateveremail>
2023-04-19 17:42:56 +1000

    hopefully fixed checkpoint troubles

--- VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/FixCameraOnRespawn.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/FixCameraOnRespawn.cs
@@ -1,10 +1,15 @@
 ﻿using HarmonyLib;
-using UnityEngine;
 
 namespace Plugin.VRTRAKILL.VRPlayer.VRCamera.Patches
 {
-    internal class FixCameraOnRespawn
+    [HarmonyPatch(typeof(NewMovement))] internal class FixCameraOnRespawn
     {
+        [HarmonyPrefix] [HarmonyPatch(nameof(NewMovement.OnEnable))] static void SetPlayerRotation(NewMovement __instance)
+        {
+            NewMovement.Instance.gameObject.transform.eulerAngles = Vars.VRCameraContainer.transform.eulerAngles; // idk if it's necessary
 
+            NewMovement.Instance.gameObject.transform.right = Vars.VRCameraContainer.transform.right;
+            NewMovement.Instance.gameObject.transform.forward = Vars.VRCameraContainer.transform.forward;
+        }
     }
 }
