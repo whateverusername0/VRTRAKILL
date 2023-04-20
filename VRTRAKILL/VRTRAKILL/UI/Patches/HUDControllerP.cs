3b64bd8d4fe88b02a1871738f4f2f960e5230a2b
whateverusername0 <whateveremail>
2023-04-20 18:30:00 +1000

    add placeholder

---
+++ VRTRAKILL/VRTRAKILL/UI/Patches/HUDControllerP.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.UI.Patches
+{
+    [HarmonyPatch(typeof(HudController))] internal class HUDControllerP
+    {
+        [HarmonyPrefix] [HarmonyPatch(nameof(HudController.Start))] static void ReparentHUD()
+        {
+
+        }
+    }
+}
