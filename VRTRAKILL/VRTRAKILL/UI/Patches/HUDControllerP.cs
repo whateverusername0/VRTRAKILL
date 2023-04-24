1eb9877b85b5990cf6b707aa9ca52478b82d75ea
whateverusername0 <whateveremail>
2023-04-24 17:00:38 +1000

    fix modern hud not present

---
+++ VRTRAKILL/VRTRAKILL/UI/Patches/HUDControllerP.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.UI.Patches
+{
+    [HarmonyPatch(typeof(HudController))] internal class HUDControllerP
+    {
+        // for some reason it starts later than other ui elements so it needs a golden fucking ticket to get converted
+        [HarmonyPostfix] [HarmonyPatch(nameof(HudController.Start))] static void ConvertCanvases(HudController __instance)
+        {
+            foreach (Canvas C in GameObject.FindObjectsOfType<Canvas>())
+                if (!Helpers.Misc.HasComponent<UICanvas>(C.gameObject))
+                    UICanvas.ConvertCanvas(C);
+        }
+    }
+}
