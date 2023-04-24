a8efbce3324bea50a8720b2484c38df9b53b919b
whateverusername0 <whateveremail>
2023-04-24 16:42:33 +1000

    clean

--- VRTRAKILL/Helpers/Misc.cs
+++ VRTRAKILL/Helpers/Misc.cs
@@ -4,7 +4,15 @@ namespace Plugin.Helpers
 {
     static class Misc
     {
-        public static bool HasComponent<T>(this GameObject Flag) where T : Component
-        { return Flag.GetComponent<T>() != null; }
+        public static bool HasComponent<T>(this GameObject GM) where T : Component
+        { return GM.GetComponent<T>() != null; }
+
+        public static void RecursiveChangeLayer(this GameObject GM, LayerMask LM)
+        {
+            GM.layer = LM;
+            if (GM.transform.childCount > 0)
+                for (int i = 0; i < GM.transform.childCount; i++)
+                    try { RecursiveChangeLayer(GM.transform.GetChild(i).gameObject, LM); } catch {}
+        }
     }
 }
