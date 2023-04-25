d5ac8da0060331f02029aebaf9071dec176c4896
whateverusername0 <whateveremail>
2023-04-25 17:52:57 +1000

    manually tweaked gun positions

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Boomstick/TransformP.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Guns.Boomstick
+{
+    [HarmonyPatch(typeof(Shotgun))] internal class TransformP
+    {
+        static Vector3 Position = new Vector3(0, -0.075f, 0.1f);
+        static Vector3 Rotation = new Vector3(0, 90, 45);
+        static Vector3 Scale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        [HarmonyPostfix] [HarmonyPatch(nameof(Shotgun.Start))] static void Retransform(Shotgun __instance)
+        {
+            __instance.wpos.defaultPos = Position;
+            __instance.wpos.defaultRot = Rotation;
+            __instance.wpos.defaultScale = Scale;
+        }
+    }
+}
---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Pistol/TransformP.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Guns.Pistol
+{
+    [HarmonyPatch(typeof(Revolver))] internal class TransformP
+    {
+        // this takes a lot of trial and error
+        static Vector3 Position = new Vector3(0, -0.075f, 0.1f);
+        static Vector3 Rotation = new Vector3(0, 90, 45);
+        static Vector3 Scale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        [HarmonyPostfix] [HarmonyPatch(nameof(Revolver.Start))] static void Retransform(Revolver __instance)
+        {
+            __instance.wpos.defaultPos = Position;
+            __instance.wpos.defaultRot = Rotation;
+            __instance.wpos.defaultScale = Scale;
+        }
+    }
+}
