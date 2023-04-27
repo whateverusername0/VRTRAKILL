52f2b45dc86dfc50c4c7a03b31ba5dcb312c63b5
whateverusername0 <whateveremail>
2023-04-27 16:15:46 +1000

    arms are not being rendered (send help)

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
@@ -5,10 +5,10 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
 {
     [HarmonyPatch(typeof(FistControl))] internal class ArmsP
     {
-        [HarmonyPrefix] [HarmonyPatch(nameof(FistControl.Start))] static void ConvertArms(FistControl __instance)
+        [HarmonyPostfix] [HarmonyPatch(nameof(FistControl.Start))] static void ConvertArms(FistControl __instance)
         {
             __instance.transform.localPosition = Vector3.zero;
-            __instance.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+            //__instance.gameObject.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
             Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
         }
     }
