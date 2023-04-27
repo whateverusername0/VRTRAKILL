179718d55247e7f83adac3602f79a51b4dbdc2a6
whateverusername0 <whateveremail>
2023-04-27 16:16:33 +1000

    no more recentering

--- VRTRAKILL/VRTRAKILL/VRPlayer/Movement/VRPlayerController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Movement/VRPlayerController.cs
@@ -16,7 +16,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.Movement
             float DistanceFromFloor = Vector3.Dot(Vars.VRCameraContainer.transform.localPosition, Vector3.up);
 
             CC.height = Mathf.Max(CC.radius, DistanceFromFloor);
-            CC.center = Vars.VRCameraContainer.transform.localPosition - 0.5f * DistanceFromFloor * Vector3.up;
+            //CC.center = Vars.VRCameraContainer.transform.localPosition - 0.5f * DistanceFromFloor * Vector3.up;
         }
     }
 }
