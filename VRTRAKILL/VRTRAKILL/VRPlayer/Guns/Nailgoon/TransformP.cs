6163be8a0a81c15039c728ba5f714c83ec6865e4
whateverusername0 <whateveremail>
2023-04-27 19:34:25 +1000

    placeholder

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Nailgoon/TransformP.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Guns.Nailgoon
+{
+    [HarmonyPatch(typeof(Nailgun))] internal class TransformP
+    {
+        static Vector3 Position = new Vector3(0, -0.075f, 0.1f);
+        static Vector3 Scale = new Vector3(0.15f, 0.15f, 0.15f);
+
+        [HarmonyPostfix] [HarmonyPatch(nameof(Nailgun.Start))] static void Retransform(Nailgun __instance)
+        {
+            __instance.wpos.defaultPos = Position;
+            __instance.wpos.defaultScale = Scale;
+        }
+    }
+}
