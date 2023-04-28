2104fb6ac2f64922a64602e05a491c3ff611e171
whateverusername0 <whateveremail>
2023-04-29 01:19:54 +1000

    fix weapon not rendering in 0-1 || after terminal

--- VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/GunsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/GunsP.cs
@@ -5,6 +5,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
 {
     [HarmonyPatch] static class GunsP
     {
+        // Disables FRT (update method will no longer be called) and adds vrgunscontroller
         [HarmonyPrefix] [HarmonyPatch(typeof(RotateToFaceFrustumTarget), nameof(RotateToFaceFrustumTarget.Update))]
         static bool DisableFrustumRotation(RotateToFaceFrustumTarget __instance)
         {
@@ -13,7 +14,13 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
             return false;
         }
 
-        [HarmonyPostfix] [HarmonyPatch(typeof(GunControl), nameof(GunControl.Start))] static void ChangeAllGOLayers(GunControl __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(GunControl), nameof(GunControl.Start))] static void ChangeGCLayers(GunControl __instance)
+        {
+            __instance.transform.localPosition = Vector3.zero;
+            Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
+        }
+        // Original method resets weapons' layers, so it changes it back to Default
+        [HarmonyPostfix] [HarmonyPatch(typeof(GunSetter), nameof(GunSetter.ResetWeapons))] static void ChangeGSLayers(GunSetter __instance)
         {
             __instance.transform.localPosition = Vector3.zero;
             Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
