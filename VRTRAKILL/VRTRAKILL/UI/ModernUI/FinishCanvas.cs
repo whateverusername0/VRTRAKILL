81a7c597bd2ccf39a0692b127edcb500c8521abf
whateverusername0 <whateveremail>
2023-04-29 01:18:24 +1000

    placeholders

--- VRTRAKILL/VRTRAKILL/UI/ModernUI/FinishCanvas.cs
+++ VRTRAKILL/VRTRAKILL/UI/ModernUI/FinishCanvas.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Plugin.VRTRAKILL.UI.ModernUI
 {
-    internal class HudPosP
+    internal class FinishCanvas
     {
         // note: add a transform patchso that it does not look so fucking big
         // and does not spawn in the middle of the screen
---
+++ VRTRAKILL/VRTRAKILL/UI/ModernUI/StyleCanvas.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.UI.ModernUI
+{
+    internal class StyleCanvas
+    {
+        // note: add a transform patchso that it does not look so fucking big
+        // and does not spawn in the middle of the screen
+    }
+}
