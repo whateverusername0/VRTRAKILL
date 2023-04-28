21d5461d81569699275d0381285bf3b92e3c045a
whateverusername0 <whateveremail>
2023-04-28 13:09:21 +1000

    impl controller aiming

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/RemoveArms.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
+{
+    // Removes hands from revolver & shotgun
+    [HarmonyPatch] static class RemoveArms
+    {
+        // Hack to leave hand only (it just makes other hitngs too small that you're unable to see them)
+        // ( Animator reverting my scaledefs is a meanie thing to do :C )
+        static Transform ArmT;
+
+        [HarmonyPrefix] [HarmonyPostfix]
+        [HarmonyPatch(typeof(Revolver), nameof(Revolver.LateUpdate))]
+        static void RemoveRevolverArm(Revolver __instance)
+        {
+            try
+            {
+                ArmT = __instance.transform.GetChild(0);
+                Arms.Feedbacker.Armature Arm = new Arms.Feedbacker.Armature(ArmT);
+                Arm.RArmature.localScale = new Vector3(1, 1, 1);
+                Arm.Hand.localScale = new Vector3(100, 100, 100);
+            }
+            catch (NullReferenceException) { Plugin.PLogger.LogWarning($"Revolver is null???"); }
+        }
+        [HarmonyPrefix] [HarmonyPostfix]
+        [HarmonyPatch(typeof(Sandbox.Arm.SandboxArm), nameof(Sandbox.Arm.SandboxArm.Update))]
+        [HarmonyPatch(typeof(Sandbox.Arm.SandboxArm), nameof(Sandbox.Arm.SandboxArm.FixedUpdate))]
+        static void RemoveSandboxArm(Sandbox.Arm.SandboxArm __instance)
+        {
+            try
+            {
+                ArmT = __instance.transform;
+                Arms.Feedbacker.Armature Arm = new Arms.Feedbacker.Armature(ArmT);
+                Arm.RArmature.localScale = new Vector3(1, 1, 1);
+                Arm.Hand.localScale = new Vector3(100, 100, 100);
+            }
+            catch (NullReferenceException) { Plugin.PLogger.LogWarning($"Sandbox arm is null???"); }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Shotgun), nameof(Shotgun.Start))]
+        static void RemoveShotgunArm(Shotgun __instance)
+        {
+            try
+            {
+                __instance.gameObject.transform.Find("Shotgun_New")
+                .gameObject.transform.Find("RightArm")
+                .gameObject.SetActive(false);
+            }
+            catch (NullReferenceException) { Plugin.PLogger.LogWarning("Shotgun is null???"); }
+        }
+    }
+}
---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/TransformP.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
+{
+    [HarmonyPatch] internal class TransformP
+    {
+        // Applies offset to guns' transforms
+        // This took a LOT of time to get those offsets right and my eyes hurt a bit
+        [HarmonyPatch(typeof(Revolver))] static class RevolverT
+        {
+            static Vector3 Position = new Vector3(0, -.1f, .6f),
+                           AltPosition = new Vector3(0, -.075f, .575f);
+            static Vector3 Scale = new Vector3(.1f, .1f, .1f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(Revolver.Start))] static void Retransform(Revolver __instance)
+            {
+                if (__instance.altVersion == true) __instance.wpos.defaultPos = AltPosition;
+                else __instance.wpos.defaultPos = Position;
+                __instance.wpos.defaultScale = Scale;
+            }
+        }
+        [HarmonyPatch(typeof(Shotgun))] static class ShotgunT
+        {
+            static Vector3 Position = new Vector3(0, .2f, .28f);
+            static Vector3 Scale = new Vector3(.1125f, .1125f, .1125f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(Shotgun.Start))] static void Retransform(Shotgun __instance)
+            {
+                __instance.wpos.defaultPos = Position;
+                __instance.wpos.defaultScale = Scale;
+            }
+        }
+        [HarmonyPatch(typeof(Nailgun))] static class NailgunT
+        {
+            static Vector3 Position = new Vector3(-.2f, .2125f, -.05f),
+                           AltPosition = new Vector3(-.25f, .35f, .075f);
+            static Vector3 Scale = new Vector3(.5f, .5f, .5f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(Nailgun.Start))] static void Retransform(Nailgun __instance)
+            {
+                if (__instance.altVersion == true) __instance.wpos.defaultPos = AltPosition;
+                else __instance.wpos.defaultPos = Position;
+                __instance.wpos.defaultScale = Scale;
+            }
+        }
+        [HarmonyPatch(typeof(Railcannon))] static class RailcannonT
+        {
+            static Vector3 Position = new Vector3(-.25f, .25f, 0);
+            static Vector3 Scale = new Vector3(.4f, .4f, .4f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(Railcannon.Start))] static void Retransform(Railcannon __instance)
+            {
+                __instance.wpos.defaultPos += Position;
+                __instance.wpos.defaultScale = Scale;
+            }
+        }
+        [HarmonyPatch(typeof(RocketLauncher))] static class RocketLauncherT
+        {
+            static Vector3 Position = new Vector3(-.3f, .3f, -.1f);
+            static Vector3 Scale = new Vector3(.75f, .75f, .75f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(RocketLauncher.Start))] static void Retransform(RocketLauncher __instance)
+            {
+                __instance.gameObject.GetComponent<WeaponPos>().defaultPos = Position;
+                __instance.gameObject.GetComponent<WeaponPos>().defaultScale = Scale;
+            }
+        }
+        [HarmonyPatch(typeof(Sandbox.Arm.SandboxArm))] static class SandboxArmT
+        {
+            static Vector3 Position = new Vector3(-.15f, .2f, -1.05f);
+            static Vector3 Scale = new Vector3(-.35f, .35f, .35f);
+
+            [HarmonyPostfix] [HarmonyPatch(nameof(Sandbox.Arm.SandboxArm.Awake))] static void Retransform(Sandbox.Arm.SandboxArm __instance)
+            {
+                __instance.transform.localPosition = Position;
+                __instance.transform.localScale = Scale;
+            }
+        }
+    }
+}
