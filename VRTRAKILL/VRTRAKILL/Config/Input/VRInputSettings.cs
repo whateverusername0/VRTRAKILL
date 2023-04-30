f4aefd0afca66b7d65f8ba3e002109e71f85b084
whateverusername0 <whateveremail>
2023-04-30 22:36:56 +1000

    moar settings + moar user friendly config

--- VRTRAKILL/VRTRAKILL/Config/Input/VRInputSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Input/VRInputSettings.cs
@@ -1,10 +1,12 @@
-﻿namespace Plugin.VRTRAKILL.Config.Input
+﻿using Newtonsoft.Json;
+
+namespace Plugin.VRTRAKILL.Config.Input
 {
     internal class VRInputSettings
     {
-        public float Deadzone { get; set; } = 0.4f;
-        public bool SnapTurning { get; set; } = false; // placeholder
-        public int SnapTurningAngles { get; set; } = 45; // placeholder
-        public int SmoothTurningSpeed { get; set; } = 300;
+        [JsonProperty("Joystick deadzone (from 0 to 1)")] public float Deadzone { get; set; } = 0.4f;
+        [JsonProperty("Snap turning (unused)")] public bool SnapTurning { get; set; } = false;
+        [JsonProperty("Snap turning angles (unused)")] public float SnapTurningAngles { get; set; } = 45;
+        [JsonProperty("Smooth turning speed")] public float SmoothTurningSpeed { get; set; } = 300;
     }
 }
--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -4,7 +4,8 @@ namespace Plugin.VRTRAKILL.Config.Settings
 {
     internal class VRSettings
     {
-        public bool EnableDefaultCrosshair { get; set; } = false; // placeholder
-        public bool DrawControllerLines { get; set; } = false; // draws helping lines, placeholder
+        [JsonProperty("Enable default crosshair")] public bool EnableDefaultCrosshair { get; set; } = true;
+        [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
+        [JsonProperty("Do not override jump strength")] public bool DoNotOverrideJumpPower { get; set; } = false;
     }
 }
