2673ec776bc49e3155ff8013af8e8dedfc44fe2d
whateverusername0 <whateveremail>
2023-05-01 20:14:05 +1000

    "improve" ik

--- VRTRAKILL/Helpers/InverseKinematics/IKManager.cs
+++ VRTRAKILL/Helpers/InverseKinematics/IKManager.cs
@@ -8,28 +8,32 @@ namespace Plugin.Helpers.InverseKinematics
         public GameObject Target;
 
         public float Threshold = 0.05f;
+        public float Rate = 20;
 
         float CalculateSlope(Joint J)
         {
             float DeltaTheta = 0.1f;
 
-            float Distance1 = GetDistance(End.transform.position, Target.transform.position);
+            float Distance1 = Vector3.Distance(End.transform.position, Target.transform.position);
             J.Rotate(DeltaTheta);
 
-            float Distance2 = GetDistance(End.transform.position, Target.transform.position);
+            float Distance2 = Vector3.Distance(End.transform.position, Target.transform.position);
             J.Rotate(-DeltaTheta);
 
             return (Distance2 - Distance1) / DeltaTheta;
         }
 
-        float GetDistance(Vector3 P1, Vector3 P2) { return Vector3.Distance(P1, P2); }
-
         void Update()
         {
-            if (GetDistance(End.transform.position, Target.transform.position) > Threshold)
+            if (Vector3.Distance(End.transform.position, Target.transform.position) > Threshold)
             {
-                float Slope = CalculateSlope(Root);
-                Root.Rotate(-Slope);
+                Joint Current = Root;
+                while (Current != null)
+                {
+                    float Slope = CalculateSlope(Root);
+                    Root.Rotate(-Slope * Rate);
+                    Current = Current.GetChild();
+                }
             }
         }
     }
