73cb374211f120a211ee71b1d21421f7660413d5
Graze <graze.the.sergal@gmail.com>
2023-05-01 15:51:13 +0100

    beams on controlers in UI

--- VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/RightArmController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/RightArmController.cs
@@ -1,25 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Plugin.VRTRAKILL.VRPlayer.Controllers
 {
     internal class RightArmController : MonoSingleton<RightArmController>
     {
         public GameObject Offset = new GameObject("Offset");
+        LineRenderer lr;
+        PointerEventData ped = new PointerEventData(EventSystem.current);
+        List<RaycastResult> results = new List<RaycastResult>();
 
         public void Start()
         {
             Offset.transform.parent = this.transform;
-            Offset.transform.localPosition = Vector3.zero;
+            Offset.transform.localPosition = new Vector3(0, 0, 1);
             Offset.transform.localRotation = Quaternion.Euler(45, 0, 0);
+
+            lr = gameObject.AddComponent<LineRenderer>();
+            lr.material = new Material(Shader.Find("GUI/Text Shader"));
+            SetLines();
+        }
+        void SetLines()
+        {
+            Color col = new Color(1, 1, 1, 0.1f);
+            Color col2 = new Color(1, 1, 1, 0.4f);
+            lr.endWidth = 0.001f;
+            lr.startWidth = 0.02f;
+            lr.startColor = col2;
+            lr.endColor = col;
+            lr.SetPosition(0, transform.GetChild(0).GetChild(2).position);
+            lr.SetPosition(1, Offset.transform.position);
         }
 
         public void Update()
         {
-            if (Vars.Config.VRSettings.DrawControllerLines)
+            ped.position = Offset.transform.position;
+            EventSystem.current.RaycastAll(ped, results);
+            if (Vars.IsAMenu)
             {
-                Debug.DrawLine(Offset.transform.position,
-                               Offset.transform.position + Offset.transform.forward * 100,
-                               Color.white, 0, false);
+                lr.enabled = true;
+                SetLines();
+            }
+            else
+            {
+                lr.enabled = false;
             }
         }
     }
