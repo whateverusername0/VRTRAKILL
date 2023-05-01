fbcd9e22dd66d9bd9c9ecd9bd71af31fd02ea620
whateverusername0 <whateveremail>
2023-05-01 20:14:13 +1000

    add more settings

--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -6,6 +6,8 @@ namespace Plugin.VRTRAKILL.Config.Settings
     {
         [JsonProperty("Enable default crosshair")] public bool EnableDefaultCrosshair { get; set; } = true;
         [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
-        [JsonProperty("Do not override jump strength")] public bool DoNotOverrideJumpPower { get; set; } = false;
+        [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
+        [JsonProperty("HUD Size (from 0 to 0.1)")] public float UISize { get; set; } = 0.0625f;
+        [JsonProperty("Create Desktop camera")] public bool CreateDesktopCam { get; set; } = true;
     }
 }
