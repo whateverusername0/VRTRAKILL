a248600c333acaa27248f53f0009beaead8c1bae
whateverusername0 <whateveremail>
2023-05-03 00:24:34 +1000

    reformat + more settings

--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -9,10 +9,24 @@ namespace Plugin.VRTRAKILL.Config.Settings
 
         [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
 
-        [JsonProperty("Draw controller lines (WIP, Unused)")] public bool DrawControllerLines { get; set; } = false;
-        [JsonProperty("Controller line initial transparency (from 0 to 1)")] public float CLInitTransparency { get; set; } = 0.4f;
-        [JsonProperty("Controller line end transparency (from 0 to 1)")] public float CLEndTransparency { get; set; } = 0.1f;
+        [JsonProperty("Controller Lines (WIP, Unused)")] public ControllerLines CL { get; set; } public class ControllerLines
+        {
+            [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
+            [JsonProperty("Controller line initial transparency (from 0 to 1)")] public float LInitTransparency { get; set; } = 0.4f;
+            [JsonProperty("Controller line end transparency (from 0 to 1)")] public float LEndTransparency { get; set; } = 0.1f;
+        }
 
-        [JsonProperty("Create Desktop camera (EXPIREMENTAL)")] public bool CreateDesktopCam { get; set; } = false;
+        [JsonProperty("Desktop View")] public DesktopView DV { get; set; } public class DesktopView
+        {
+            [JsonProperty("Enable Desktop View (for recording, etc.)")] public bool EnableDV { get; set; } = false;
+            [JsonProperty("World View FOV")] public float WorldCamFOV { get; set; } = 90;
+            [JsonProperty("UI View FOV")] public float UICamFOV { get; set; } = 45;
+        }
+
+        public VRSettings()
+        {
+            CL = new ControllerLines();
+            DV = new DesktopView();
+        }
     }
 }
