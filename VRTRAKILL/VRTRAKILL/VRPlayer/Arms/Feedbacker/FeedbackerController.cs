f89037935589a4253df8cd827a1a9048fee3450f
whateverusername0 <whateveremail>
2023-05-03 00:24:45 +1000

    add palceholders

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Feedbacker/FeedbackerController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Feedbacker/FeedbackerController.cs
@@ -5,16 +5,27 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms.Feedbacker
 {
     internal class FeedbackerController : MonoSingleton<FeedbackerController>
     {
-        public Transform PunchZoneT;
+        public Transform PunchZoneT; public Armature Arm;
+
+        private void UpdateTransform()
+        {
+            Arm.Hand.position = Vars.LeftController.transform.position;
+            Arm.Hand.rotation = Vars.LeftController.transform.rotation;
+        }
 
         void Start()
         {
 
         }
-
         void Update()
         {
-
+            UpdateTransform();
+        }
+        // Override Animator shenanigans
+        void LateUpdate()
+        {
+            Arm.UpperArm.localScale /= 100; Arm.Hand.localScale *= 100;
+            UpdateTransform();
         }
     }
 }
