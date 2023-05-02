f385b9ea84fd214cd902ac66184269b8ae543a81
whateverusername0 <whateveremail>
2023-05-02 20:07:12 +1000

    change config property name

--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -9,7 +9,7 @@ namespace Plugin.VRTRAKILL.Config.Settings
 
         [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
 
-        [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
+        [JsonProperty("Draw controller lines (WIP, Unused)")] public bool DrawControllerLines { get; set; } = false;
         [JsonProperty("Controller line initial transparency (from 0 to 1)")] public float CLInitTransparency { get; set; } = 0.4f;
         [JsonProperty("Controller line end transparency (from 0 to 1)")] public float CLEndTransparency { get; set; } = 0.1f;
 
