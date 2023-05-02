61c0bc884fae994f98cdda93bad80061b92e2cb5
whateverusername0 <whateveremail>
2023-05-02 20:09:07 +1000

    fix bug when fists models disappear after "n"

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
@@ -5,7 +5,10 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
 {
     [HarmonyPatch(typeof(FistControl))] internal class ArmsP
     {
-        [HarmonyPostfix] [HarmonyPatch(nameof(FistControl.Start))] static void ConvertArms(FistControl __instance)
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(FistControl.Start))]
+        [HarmonyPatch(nameof(FistControl.ResetFists))]
+        static void ConvertArms(FistControl __instance)
         {
             __instance.transform.localPosition = Vector3.zero;
             //__instance.gameObject.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
