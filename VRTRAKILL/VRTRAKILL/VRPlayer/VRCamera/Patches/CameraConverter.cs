e0726c1daf9f4b7900638ac5f85e82ef087d604a
whateverusername0 <whateveremail>
2023-05-03 12:46:27 +1000

    order matters

--- VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/CameraConverter.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/CameraConverter.cs
@@ -28,20 +28,20 @@ namespace Plugin.VRTRAKILL.VRPlayer.VRCamera.Patches
                 DesktopWorldCam.transform.localPosition = Vector3.zero;
                 DesktopWorldCam.nearClipPlane = 0.1f;
                 DesktopWorldCam.depth = 69;
-                DesktopWorldCam.fieldOfView = Vars.Config.VRSettings.DV.WorldCamFOV;
-                DesktopWorldCam.clearFlags = Vars.MainCamera.clearFlags;
-                DesktopWorldCam.cullingMask = Vars.MainCamera.cullingMask & Vars.VRUICamera.cullingMask;
                 DesktopWorldCam.stereoTargetEye = StereoTargetEyeMask.None;
+                DesktopWorldCam.cullingMask = Vars.MainCamera.cullingMask & Vars.VRUICamera.cullingMask;
+                DesktopWorldCam.clearFlags = Vars.MainCamera.clearFlags;
+                DesktopWorldCam.fieldOfView = Vars.Config.VRSettings.DV.WorldCamFOV;
 
                 DesktopUICam = new GameObject("Desktop UI Camera").AddComponent<Camera>();
                 DesktopUICam.transform.parent = Vars.VRUICamera.transform;
                 DesktopUICam.transform.localPosition = Vector3.zero;
                 DesktopUICam.nearClipPlane = 0.1f;
                 DesktopUICam.depth = 70;
-                DesktopUICam.fieldOfView = Vars.Config.VRSettings.DV.UICamFOV;
-                DesktopUICam.clearFlags = Vars.VRUICamera.clearFlags;
-                DesktopUICam.cullingMask = Vars.VRUICamera.cullingMask;
                 DesktopUICam.stereoTargetEye = StereoTargetEyeMask.None;
+                DesktopUICam.cullingMask = Vars.VRUICamera.cullingMask;
+                DesktopUICam.clearFlags = Vars.VRUICamera.clearFlags;
+                DesktopUICam.fieldOfView = Vars.Config.VRSettings.DV.UICamFOV;
             }
         }
 
