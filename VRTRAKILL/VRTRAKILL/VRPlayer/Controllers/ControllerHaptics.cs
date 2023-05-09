52095dd896a9ba3a27e5795dcff606a457557da9
whateverusername0 <whateveremail>
2023-05-09 22:23:38 +1000

    placeholders

--- VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/ControllerHaptics.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/ControllerHaptics.cs
@@ -1,23 +1,55 @@
-﻿using UnityEngine;
+﻿using HarmonyLib;
+using UnityEngine;
 using Valve.VR;
 
 namespace Plugin.VRTRAKILL.VRPlayer.Controllers
 {
-    internal class ControllerHaptics : MonoBehaviour
+    [HarmonyPatch(typeof(RumbleManager))] internal class ControllerHaptics
     {
-        // uses existing RumbleManager
-        float Duration = 0,
-              Frequency = 0,
-              Amplitude = 0;
-
-        public void Update()
+        [HarmonyPostfix] [HarmonyPatch(nameof(RumbleManager.Update))] static void Update()
         {
-            Pulse(Duration, Frequency, Amplitude, SteamVR_Input_Sources.Any);
+
+        }
+        [HarmonyPostfix][HarmonyPatch(nameof(RumbleManager.OnDisable))] static void OnDisable()
+        {
+
         }
 
-        private void Pulse(float Duration, float Frequency, float Amplitude, SteamVR_Input_Sources Source)
+        public SteamVR_Input_Sources ResolveController(string Key)
         {
+            switch (Key)
+            {
+                case "rumble.slide":
+                case "rumble.dash":
+                case "rumble.fall_impact":
+                case "rumble.jump":
+                case "rumble.fall_impact_heave":
+                case "rumble.weapon_wheel_tick":
+                    return SteamVR_Input_Sources.Any;
 
+                case "rumble.punch":
+                case "rumble.parry_flash":
+                case "rumble.coin_toss":
+                case "rumble.whiplash.throw":
+                case "rumble.whiplash.pull":
+                    return SteamVR_Input_Sources.LeftHand;
+
+                case "rumble.gun.fire":
+                case "rumble.gun.fire_strong":
+                case "rumble.gun.fire_projectiles":
+                case "rumble.gun.railcannon_idle":
+                case "rumble.gun.nailgun_fire":
+                case "rumble.gun.super_saw":
+                case "rumble.gun.shotgun_charge":
+                case "rumble.gun.sawblade":
+                case "rumble.gun.revolver_charge":
+                case "rumble.magnet_released":
+                    return SteamVR_Input_Sources.RightHand;
+
+                default:
+                    Debug.LogError("No intensity found for key: " + Key);
+                    return SteamVR_Input_Sources.Any;
+            }
         }
     }
 }
