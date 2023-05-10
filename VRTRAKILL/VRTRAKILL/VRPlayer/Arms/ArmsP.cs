87f4a1753b53e9f83c1fc3575c743a7a9a1806fe
whateverusername0 <whateveremail>
2023-05-10 15:57:36 +1000

    arms a lil bit farther away

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
@@ -10,8 +10,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
         [HarmonyPatch(nameof(FistControl.ResetFists))]
         static void ConvertArms(FistControl __instance)
         {
-            __instance.transform.localPosition = Vector3.zero;
-            //__instance.gameObject.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+            __instance.transform.localPosition = new Vector3(0, 0, .25f);
             Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
         }
     }
