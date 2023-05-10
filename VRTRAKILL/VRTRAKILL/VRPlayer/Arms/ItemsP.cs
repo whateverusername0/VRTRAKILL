88804e5ab358669cd134730ca238f95ccd15cf51
whateverusername0 <whateveremail>
2023-05-10 15:57:43 +1000

    placeholder

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ItemsP.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms
+{
+    [HarmonyPatch] internal class ItemsP
+    {
+        [HarmonyPostfix] [HarmonyPatch(typeof(Skull), nameof(Skull.PunchWith))] static void ReLayerSkull(Skull __instance)
+        {
+            //Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
+        }
+    }
+}
