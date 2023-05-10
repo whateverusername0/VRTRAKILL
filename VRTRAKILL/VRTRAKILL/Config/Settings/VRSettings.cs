acac0a808a5011d002725af079b50d1630420d48
whateverusername0 <whateveremail>
2023-05-10 14:30:54 +1000

    config names tweaking

--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -4,21 +4,21 @@ namespace Plugin.VRTRAKILL.Config.Settings
 {
     internal class VRSettings
     {
-        [JsonProperty("Enable default crosshair")] public bool EnableDefaultCrosshair { get; set; } = true;
+        [JsonProperty("Enable default crosshair")] public bool EnableDefaultCrosshair { get; set; } = false;
         [JsonProperty("HUD Size (from 0 to 0.1)")] public float UISize { get; set; } = 0.0625f;
 
         [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
 
-        [JsonProperty("Controller Lines (WIP, Unused)")] public ControllerLines CL { get; set; } public class ControllerLines
+        [JsonProperty("Controller Lines (unused)")] public ControllerLines CL { get; set; } public class ControllerLines
         {
             [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
             [JsonProperty("Controller line initial transparency (from 0 to 1)")] public float LInitTransparency { get; set; } = 0.4f;
             [JsonProperty("Controller line end transparency (from 0 to 1)")] public float LEndTransparency { get; set; } = 0.1f;
         }
 
-        [JsonProperty("Desktop View")] public DesktopView DV { get; set; } public class DesktopView
+        [JsonProperty("Desktop View (for recording, etc.)")] public DesktopView DV { get; set; } public class DesktopView
         {
-            [JsonProperty("Enable Desktop View (for recording, etc.)")] public bool EnableDV { get; set; } = true;
+            [JsonProperty("Enable Desktop View")] public bool EnableDV { get; set; } = true;
             [JsonProperty("World View FOV")] public float WorldCamFOV { get; set; } = 90;
             [JsonProperty("UI View FOV")] public float UICamFOV { get; set; } = 45;
         }
