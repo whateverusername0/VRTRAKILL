30b9e993c2b03562f25ec3902db4e31f77dd803f
whateverusername0 <whateveremail>
2023-05-13 01:37:02 +1000

    fix typo in property name

--- VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/VRSettings.cs
@@ -8,7 +8,7 @@ namespace Plugin.VRTRAKILL.Config.Settings
         {
             [JsonProperty("Enable default crosshair")] public bool EnableDefaultCrosshair { get; set; } = false;
             [JsonProperty("HUD Size (from 0 to 0.1)")] public float UISize { get; set; } = 0.0625f;
-            [JsonProperty("Enabke standard HUD")] public bool EnableStandardHUD { get; set; } = false;
+            [JsonProperty("Enable standard HUD")] public bool EnableStandardHUD { get; set; } = false;
         }
 
         [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
