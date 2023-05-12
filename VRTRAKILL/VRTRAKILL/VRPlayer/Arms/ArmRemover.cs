92d94fb34aba788bcf878b9eb46a6daef0b4b3f2
whateverusername0 <whateveremail>
2023-05-13 01:42:11 +1000

    baliff, deploy armremover!

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmRemover.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmRemover.cs
@@ -6,27 +6,45 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
     internal class ArmRemover : MonoBehaviour
     {
         Transform Armature;
-        Feedbacker.Armature FArm;
+        Feedbacker.Armature FBArm; Knuckleblaster.Armature KBArm;
         Vector3 ArmSize, HandSize;
         public void Start()
         {
             if (gameObject.HasComponent<Revolver>())
             {
                 Armature = transform.GetChild(0);
-                FArm = new Feedbacker.Armature(Armature);
+                FBArm = new Feedbacker.Armature(Armature);
                 ArmSize = new Vector3(1, 1, 1); HandSize = new Vector3(100, 100, 100);
             }
-            if (gameObject.HasComponent<Sandbox.Arm.SandboxArm>())
+            else if (gameObject.HasComponent<Sandbox.Arm.SandboxArm>())
             {
                 Armature = transform;
-                FArm = new Feedbacker.Armature(Armature);
-                ArmSize = new Vector3(.01f, .01f, .01f); HandSize = new Vector3(10, 10, 10);
+                FBArm = new Feedbacker.Armature(Armature);
+                ArmSize = new Vector3(1, 1, 1); HandSize = new Vector3(100, 100, 100);
+            }
+            else if (gameObject.HasComponent<Punch>())
+            {
+                switch(GetComponent<Punch>().type)
+                {
+                    case FistType.Standard:
+                        //Armature = transform.GetChild(0);
+                        //FBArm = new Feedbacker.Armature(Armature);
+                        //ArmSize = new Vector3(.01f, .01f, .01f); HandSize = new Vector3(100, 100, 100);
+                        break;
+                    case FistType.Heavy:
+                        break;
+                    case FistType.Spear:
+                        break;
+                }
             }
         }
         public void LateUpdate()
         {
-            FArm.RArmature.localScale = ArmSize;
-            FArm.Hand.localScale = HandSize;
+            try
+            {
+                FBArm.RArmature.localScale = ArmSize;
+                FBArm.Hand.localScale = HandSize;
+            } catch {}
         }
     }
 }
