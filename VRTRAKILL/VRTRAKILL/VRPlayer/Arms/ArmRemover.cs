c52226f81cc814f1b35691e2e087425b2313b83d
whateverusername0 <whateveremail>
2023-05-12 20:49:54 +1000

    specify Arm as FArm (feedbacker)

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmRemover.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmRemover.cs
@@ -5,27 +5,28 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
 {
     internal class ArmRemover : MonoBehaviour
     {
-        Transform Armature; Feedbacker.Armature Arm;
+        Transform Armature;
+        Feedbacker.Armature FArm;
         Vector3 ArmSize, HandSize;
         public void Start()
         {
             if (gameObject.HasComponent<Revolver>())
             {
                 Armature = transform.GetChild(0);
-                Arm = new Feedbacker.Armature(Armature);
+                FArm = new Feedbacker.Armature(Armature);
                 ArmSize = new Vector3(1, 1, 1); HandSize = new Vector3(100, 100, 100);
             }
             if (gameObject.HasComponent<Sandbox.Arm.SandboxArm>())
             {
                 Armature = transform;
-                Arm = new Feedbacker.Armature(Armature);
-                ArmSize = new Vector3(.01f, .01f, .01f); HandSize = new Vector3(100, 100, 100);
+                FArm = new Feedbacker.Armature(Armature);
+                ArmSize = new Vector3(.01f, .01f, .01f); HandSize = new Vector3(10, 10, 10);
             }
         }
         public void LateUpdate()
         {
-            Arm.RArmature.localScale = ArmSize;
-            Arm.Hand.localScale = HandSize;
+            FArm.RArmature.localScale = ArmSize;
+            FArm.Hand.localScale = HandSize;
         }
     }
 }
