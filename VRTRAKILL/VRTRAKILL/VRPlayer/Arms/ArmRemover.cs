342b7fe32ab5dc3133196226354aba5b3ac1720e
whateverusername0 <whateveremail>
2023-05-12 10:56:27 +1000

    new arm remover

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmRemover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Plugin.Helpers;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms
+{
+    internal class ArmRemover : MonoBehaviour
+    {
+        Transform Armature; Feedbacker.Armature Arm;
+        Vector3 ArmSize, HandSize;
+        public void Start()
+        {
+            if (gameObject.HasComponent<Revolver>())
+            {
+                Armature = transform.GetChild(0);
+                Arm = new Feedbacker.Armature(Armature);
+                ArmSize = new Vector3(1, 1, 1); HandSize = new Vector3(100, 100, 100);
+            }
+            if (gameObject.HasComponent<Sandbox.Arm.SandboxArm>())
+            {
+                Armature = transform;
+                Arm = new Feedbacker.Armature(Armature);
+                ArmSize = new Vector3(.01f, .01f, .01f); HandSize = new Vector3(100, 100, 100);
+            }
+        }
+        public void LateUpdate()
+        {
+            Arm.RArmature.localScale = ArmSize;
+            Arm.Hand.localScale = HandSize;
+        }
+    }
+}
--- VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/RemoveArms.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/RemoveArms.cs
@@ -12,36 +12,19 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
         // ( Animator reverting my scaledefs is a meanie thing to do :C )
         static Transform ArmT;
 
-        [HarmonyPrefix] [HarmonyPostfix] [HarmonyPatch(typeof(Revolver), nameof(Revolver.LateUpdate))]
+        [HarmonyPostfix] [HarmonyPatch(typeof(Revolver), nameof(Revolver.Start))]
         static void RemoveRevolverArm(Revolver __instance)
         {
-            try
-            {
-                ArmT = __instance.transform.GetChild(0);
-                Arms.Feedbacker.Armature Arm = new Arms.Feedbacker.Armature(ArmT);
-                Arm.RArmature.localScale = new Vector3(1, 1, 1);
-                Arm.Hand.localScale = new Vector3(100, 100, 100);
-            }
+            try { __instance.gameObject.AddComponent<Arms.ArmRemover>(); }
             catch (NullReferenceException) { Plugin.PLogger.LogWarning($"Revolver is null???"); }
         }
-        [HarmonyPrefix] [HarmonyPostfix]
-        [HarmonyPatch(typeof(SandboxArm), nameof(SandboxArm.Update))]
-        [HarmonyPatch(typeof(SandboxArm), nameof(SandboxArm.FixedUpdate))]
-        static void RemoveSandboxArm(SandboxArm __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(SandboxArm), nameof(SandboxArm.Awake))] static void RemoveSandboxArm(SandboxArm __instance)
         {
-            try
-            {
-                ArmT = __instance.transform;
-                Arms.Feedbacker.Armature Arm = new Arms.Feedbacker.Armature(ArmT);
-                Arm.RArmature.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-                Arm.Hand.localScale = new Vector3(1, 1, 1);
-            }
+            try { __instance.gameObject.AddComponent<Arms.ArmRemover>(); }
             catch (NullReferenceException) { Plugin.PLogger.LogWarning($"Sandbox arm is null???"); }
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(Shotgun), nameof(Shotgun.Start))]
-        static void RemoveShotgunArm(Shotgun __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(Shotgun), nameof(Shotgun.Start))] static void RemoveShotgunArm(Shotgun __instance)
         {
             try
             {
