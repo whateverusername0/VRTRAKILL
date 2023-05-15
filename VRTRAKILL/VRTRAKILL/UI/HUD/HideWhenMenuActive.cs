204c4a3c9c3e1cf3d6beea9fae6bb0d849d71492
whateverusername0 <whateveremail>
2023-05-16 00:50:57 +1000

    change hide handling

--- VRTRAKILL/VRTRAKILL/UI/HUD/HideWhenMenuActive.cs
+++ VRTRAKILL/VRTRAKILL/UI/HUD/HideWhenMenuActive.cs
@@ -4,9 +4,11 @@ namespace Plugin.VRTRAKILL.UI.HUD
 {
     internal class HideWhenMenuActive : MonoBehaviour
     {
+        private bool ShouldHide => Vars.IsAMenu || NewMovement.Instance.dead;
+
         public void Update()
         {
-            if (Vars.IsAMenu) gameObject.GetComponent<Canvas>().enabled = false;
+            if (ShouldHide) gameObject.GetComponent<Canvas>().enabled = false;
             else gameObject.GetComponent<Canvas>().enabled = true;
         }
     }
