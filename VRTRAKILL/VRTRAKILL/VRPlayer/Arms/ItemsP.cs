562662fea5a11f3e25841ad0e0066135000ae71c
whateverusername0 <whateveremail>
2023-05-16 00:52:18 +1000

    implemented items drawing (skulls, etc.)

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ItemsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ItemsP.cs
@@ -1,12 +1,31 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace Plugin.VRTRAKILL.VRPlayer.Arms
 {
     [HarmonyPatch] internal class ItemsP
     {
-        [HarmonyPostfix] [HarmonyPatch(typeof(Skull), nameof(Skull.PunchWith))] static void ReLayerSkull(Skull __instance)
+        static ItemIdentifier ItemID { get; set; }
+        static LayerMask ItemLM { get; set; }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Punch), nameof(Punch.Update))]
+        [HarmonyPatch(typeof(Punch), nameof(Punch.PunchEnd))]
+        static void ReLayerHeldItems(Punch __instance)
         {
-            //Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
+            if (__instance.heldItem != null)
+            {
+                ItemID = __instance.heldItem; ItemLM = ItemID.gameObject.layer;
+                ItemID.gameObject.layer = 0;
+            }
+        }
+        [HarmonyPrefix] [HarmonyPatch(typeof(Punch), nameof(Punch.PunchStart))] static void LayerBackHeldItems(Punch __instance)
+        {
+            if (ItemID != null && __instance.heldItem == null)
+            {
+                ItemID.gameObject.layer = ItemLM;
+                ItemID = null;
+            }
         }
     }
 }
