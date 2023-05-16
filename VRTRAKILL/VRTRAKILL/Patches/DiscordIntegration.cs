5e59055bb40a85296757132bad75bc7eeff25fed
whateverusername0 <whateveremail>
2023-05-16 21:28:37 +1000

    shitty discord integration

---
+++ VRTRAKILL/VRTRAKILL/Patches/DiscordIntegration.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Plugin.VRTRAKILL.Patches
+{
+    [HarmonyPatch(typeof(DiscordController))] internal class DiscordIntegration
+    {
+        [HarmonyPostfix] [HarmonyPatch(nameof(DiscordController.Awake))] public void SendActivity(DiscordController __instance)
+        {
+            // that's it, lol
+            __instance.cachedActivity.Name = "VRTRAKILL";
+        }
+    }
+}
