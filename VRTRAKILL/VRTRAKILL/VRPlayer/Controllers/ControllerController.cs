6b42a3c304ed9793cf51c430245f97ff610451b3
whateverusername0 <whateveremail>
2023-05-21 03:19:29 +1000

    made updatecontrollertransform more clean for further modification

--- VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/ControllerController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Controllers/ControllerController.cs
@@ -79,11 +79,10 @@ namespace Plugin.VRTRAKILL.VRPlayer.Controllers
                 DrawControllerLines();
         }
 
+        public static Vector3 ControllerOffset = new Vector3(0, 1.4f, 0);
         public static void onTransformUpdatedH(SteamVR_Behaviour_Pose fromAction, SteamVR_Input_Sources fromSource)
         {
-            fromAction.transform.position = new Vector3(fromAction.transform.position.x,
-                                                        fromAction.transform.position.y + 1.4f,
-                                                        fromAction.transform.position.z);
+            fromAction.transform.position += ControllerOffset;
         }
     }
 }
