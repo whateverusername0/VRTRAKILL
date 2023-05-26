8b04419a5157fb6468fd66c842a7390ca0b99da4
whateverusername0 <whateveremail>
2023-05-26 23:54:28 +1000

    increase fistcontrol scale (dunno why)

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/ArmsP.cs
@@ -10,6 +10,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
         [HarmonyPatch(nameof(FistControl.ResetFists))]
         static void ConvertArms(FistControl __instance)
         {
+            __instance.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
             __instance.transform.localPosition = new Vector3(0, 0, .25f);
             Helpers.Misc.RecursiveChangeLayer(__instance.gameObject, 0);
         }
