e15abc3a9a3ae567a69917fd32cfa848177ddb11
whateverusername0 <whateveremail>
2023-05-26 23:54:09 +1000

    implement steamvr_camera patch

---
+++ VRTRAKILL/VRTRAKILL/Patches/SteamVRP.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Valve.VR;
+
+namespace Plugin.VRTRAKILL.Patches
+{
+    [HarmonyPatch] internal class SteamVRP
+    {
+        [HarmonyPrefix] [HarmonyPatch(typeof(SteamVR_Camera), nameof(SteamVR_Camera.Expand))]
+        static bool SVRCExpand(SteamVR_Camera __instance, Transform ____ears, Transform ____head)
+        {
+            // I can't believe i'm patching a whole ass svr method to fit my needs. oh well
+            __instance.gameObject.AddComponent<SteamVR_Ears>();
+            ____ears = __instance.transform;
+
+            //__instance.gameObject.AddComponent<SteamVR_TrackedObject>();
+            ____head = __instance.transform;
+
+            return false;
+        }
+    }
+}
