7978fc6269f95fd06776ea1192b1c98d3f69c970
whateverusername0 <whateveremail>
2023-05-30 00:48:05 +1000

    add kbc placeholder

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Knuckleblaster/KnuckleblasterController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms.Knuckleblaster
+{
+    internal class KnuckleblasterController : MonoBehaviour
+    {
+        public Armature Arm;
+        public Vector3 OffsetPosition = new Vector3(0, 0, 0), TotalPos = Vector3.zero;
+        public Quaternion OffsetRotation = Quaternion.Euler(0, 0, 0);
+
+        public void Start()
+        {
+
+        }
+
+        public void Update() { LateUpdate(); }
+        public void LateUpdate()
+        {
+
+        }
+    }
+}
