43f46f4aea29ec01fca055011de99ae65b8a02c5
whateverusername0 <whateveremail>
2023-05-30 02:12:32 +1000

    cleared up bugged code

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Feedbacker/FeedbackerController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Feedbacker/FeedbackerController.cs
@@ -6,23 +6,16 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms.Feedbacker
     {
         public Armature Arm;
         public Vector3 OffsetPosition = new Vector3(0, 0, 0), TotalPos = Vector3.zero;
-        public Quaternion OffsetRotation = Quaternion.Euler(0, 180, 0);
+        public Quaternion OffsetRotation = Quaternion.Euler(-45, 180, 0);
 
         public void Start()
         {
-            Arm.Hand.position = OffsetPosition;
-            Arm.Hand.rotation = OffsetRotation;
-        }
 
+        }
+        public void Update() { LateUpdate(); }
         public void LateUpdate()
         {
-            TotalPos = Vars.LeftController.transform.position + OffsetPosition;
 
-            Arm.Root.position = TotalPos;
-            //Arm.Root.rotation = Vars.LeftController.transform.rotation;
-
-            //Arm.Hand.position = TotalPos;
-            Arm.Hand.rotation = Vars.LeftController.transform.rotation * OffsetRotation;
         }
     }
 }
