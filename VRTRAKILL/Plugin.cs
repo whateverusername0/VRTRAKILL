5b6b1c726c7b05d76b36e08038564caace2f1736
whateverusername0 <whateveremail>
2023-06-02 15:52:51 +1000

    changed plugin version to 0.9

--- VRTRAKILL/Plugin.cs
+++ VRTRAKILL/Plugin.cs
@@ -4,11 +4,15 @@ using Valve.VR;
 
 namespace Plugin
 {
+    /* If you're reading this and don't plan on stopping, then
+     * welcome to the codebase where all hopes and dreams go die.
+     * This is a dumpster fire of spaghetti code, inconsistent
+     * naming and questionable life choices. Amen. */
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)] public class Plugin : BaseUnityPlugin
     {
         public const string PLUGIN_GUID = "com.whateverusername0.vrtrakill",
                             PLUGIN_NAME = "VRTRAKILL",
-                            PLUGIN_VERSION = "0.8.6";
+                            PLUGIN_VERSION = "0.9";
 
         public static string GameExePath = Process.GetCurrentProcess().MainModule.FileName,
                              GamePath = Path.GetDirectoryName(GameExePath),
