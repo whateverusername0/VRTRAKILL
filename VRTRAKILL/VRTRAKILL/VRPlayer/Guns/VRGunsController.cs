565fdd0a0dda98b1d28c9eb86005c940dd1ce735
whateverusername0 <whateveremail>
2023-06-03 02:21:52 +1000

    removed unnecessary code

--- VRTRAKILL/VRTRAKILL/VRPlayer/Guns/VRGunsController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/VRGunsController.cs
@@ -4,11 +4,6 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns
 {
     internal class VRGunsController : MonoBehaviour
     {
-        public void Start()
-        {
-            GetComponent<WalkingBob>().enabled = false;
-        }
-
         public void Update()
         {
             transform.position = Vars.RightController.transform.position;
