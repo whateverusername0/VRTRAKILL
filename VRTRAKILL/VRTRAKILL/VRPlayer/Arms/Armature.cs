0a4337979913ab162c93817dad6be91725bae530
whateverusername0 <whateveremail>
2023-06-04 23:06:56 +1000

    comments

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Armature.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Armature.cs
@@ -38,7 +38,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
         public Transform Root { get; }
         public Transform Clavicle { get; }
         public Transform UpperArm => Clavicle.GetChild(0); // * special case * //
-        public Transform LowerArm => UpperArm.GetChild(0);
+        public Transform LowerArm => UpperArm.GetChild(0); // * special case * //
         public Transform Wrist { get; }
 
         public Transform Hand { get; }
