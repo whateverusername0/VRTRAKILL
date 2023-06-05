9fba5462b9bef584ae8e73e317ce8fdbf84c8e44
whateverusername0 <whateveremail>
2023-06-05 20:48:03 +1000

    added a placeholder so that the compiler doesn't get angry at me

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/VRIKPatches/Placeholder.cs
@@ -0,0 +1,9 @@
+﻿using HarmonyLib;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms.VRIKPatches
+{
+    [HarmonyPatch] internal class Placeholder
+    {
+        // This is a placeholder just so that the compiler doesn't get angry
+    }
+}
