6ee9de9de327482327da968b1d58dec70dfdc872
whateverusername0 <whateveremail>
2023-06-05 23:44:19 +1000

    fix middlepos big gun

--- VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/TransformP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Guns/Patches/TransformP.cs
@@ -101,5 +101,60 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
                 }
             }
         }
+
+        // Exclude middlepos (or middlepos big gun fix)
+        [HarmonyPatch(typeof(WeaponPos), nameof(WeaponPos.CheckPosition))] static bool ExcludeMiddlePos(WeaponPos __instance)
+        {
+            if (!__instance.ready)
+            {
+                __instance.ready = true;
+                __instance.defaultPos = __instance.transform.localPosition;
+                __instance.defaultRot = __instance.transform.localRotation.eulerAngles;
+                __instance.defaultScale = __instance.transform.localScale;
+
+                if (__instance.middleScale == Vector3.zero) __instance.middleScale = __instance.defaultScale;
+                if (__instance.middleRot == Vector3.zero) __instance.middleRot = __instance.defaultRot;
+                //if (__instance.moveOnMiddlePos != null && __instance.moveOnMiddlePos.Length != 0)
+                //{
+                //    for (int i = 0; i < __instance.moveOnMiddlePos.Length; i++)
+                //    {
+                //        __instance.defaultPosValues.Add(__instance.moveOnMiddlePos[i].localPosition);
+                //        __instance.defaultRotValues.Add(__instance.moveOnMiddlePos[i].localEulerAngles);
+                //        if (__instance.middleRotValues[i] == Vector3.zero)
+                //            __instance.middleRotValues[i] = __instance.moveOnMiddlePos[i].localEulerAngles;
+                //    }
+                //}
+            }
+            //if (MonoSingleton<PrefsManager>.Instance.GetInt("weaponHoldPosition", 0) == 1 && (!MonoSingleton<PowerUpMeter>.Instance || MonoSingleton<PowerUpMeter>.Instance.juice <= 0f))
+            //{
+            //    __instance.transform.localPosition = __instance.middlePos;
+            //    __instance.transform.localRotation = Quaternion.Euler(__instance.middleRot);
+            //    __instance.transform.localScale = __instance.middleScale;
+            //    if (__instance.moveOnMiddlePos != null && __instance.moveOnMiddlePos.Length != 0)
+            //    {
+            //        for (int j = 0; j < __instance.moveOnMiddlePos.Length; j++)
+            //        {
+            //            __instance.moveOnMiddlePos[j].localPosition = __instance.middlePosValues[j];
+            //            __instance.moveOnMiddlePos[j].localEulerAngles = __instance.middleRotValues[j];
+            //        }
+            //    }
+            //}
+            //else
+            //{
+                __instance.transform.localPosition = __instance.defaultPos;
+                __instance.transform.localRotation = Quaternion.Euler(__instance.defaultRot);
+                __instance.transform.localScale = __instance.defaultScale;
+                //if (__instance.moveOnMiddlePos != null && __instance.moveOnMiddlePos.Length != 0)
+                //{
+                //    for (int k = 0; k < __instance.moveOnMiddlePos.Length; k++)
+                //    {
+                //        __instance.moveOnMiddlePos[k].localPosition = __instance.defaultPosValues[k];
+                //        __instance.moveOnMiddlePos[k].localEulerAngles = __instance.defaultRotValues[k];
+                //    }
+                //}
+            //}
+            __instance.currentDefault = __instance.transform.localPosition;
+            return false;
+        }
     }
 }
