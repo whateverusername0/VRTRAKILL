c8456ddefa0c0f726e31d267fb15cd2d8de2c99e
whateverusername0 <whateveremail>
2023-06-06 00:56:31 +1000

    change some of the controllerlines settings names

--- VRTRAKILL/VRTRAKILL/Config/Settings/Controllers/ControllerLines.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/Controllers/ControllerLines.cs
@@ -4,7 +4,7 @@ namespace Plugin.VRTRAKILL.Config.Settings.Controllers
 {
     public class ControllerLines
     {
-        [JsonProperty("Draw controller lines (Replaces Gaze interaction. BROKEN.)")] public bool DrawControllerLines { get; set; } = false;
+        [JsonProperty("Draw controller lines")] public bool DrawControllerLines { get; set; } = false;
         [JsonProperty("Initial transparency (from 0 to 1)")] public float LInitTransparency { get; set; } = 0.4f;
         [JsonProperty("End transparency (from 0 to 1)")] public float LEndTransparency { get; set; } = 0.1f;
     }
