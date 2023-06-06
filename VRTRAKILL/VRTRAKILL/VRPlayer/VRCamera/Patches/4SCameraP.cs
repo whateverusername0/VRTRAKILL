e850e554eb991c64266aac1af6f467a5580f327e
whateverusername0 <whateveremail>
2023-06-06 14:58:21 +1000

    made a thing

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRCamera/Patches/4SCameraP.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Valve.VR;
+using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.VRCamera.Patches
+{
+    [HarmonyPatch] internal class _4SCameraP
+    {
+        static GameObject Container;
+        [HarmonyPrefix] [HarmonyPatch(typeof(CameraController), nameof(CameraController.Start))] static void Convert4SCam(CameraController __instance)
+        {
+            if (__instance.platformerCamera)
+            {
+                // Containerize
+                Container = new GameObject("CamRig");
+                Container.transform.parent = __instance.transform.parent.parent;
+                __instance.transform.parent.parent = Container.transform;
+                Container.AddComponent<VRCameraController>();
+
+                __instance.gameObject.AddComponent<SteamVR_Camera>();
+            }
+        }
+
+        [HarmonyPrefix] [HarmonyPatch(typeof(CameraController), nameof(CameraController.Start))] static void FirstPerson4SCam(CameraController __instance)
+        {
+            if (__instance.platformerCamera && Vars.Config.Game.EnableFP4SCam)
+            { Container.transform.localPosition = new Vector3(0, 0, 0); }
+        }
+    }
+}
