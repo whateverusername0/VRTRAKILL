476a6611991b25985b12b4facfca43c2fee4fbf7
whateverusername0 <whateveremail>
2023-06-06 14:58:09 +1000

    added fp4scam setting (unused)

--- VRTRAKILL/VRTRAKILL/Config/Settings/Game.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/Game.cs
@@ -5,5 +5,6 @@ namespace Plugin.VRTRAKILL.Config.Settings
     public class Game
     {
         [JsonProperty("Do not override movement values")] public bool DoNotOverrideMoveValues { get; set; } = false;
+        [JsonProperty("Enable first-person 4-S camera")] public bool EnableFP4SCam { get; set; } = true;
     }
 }
