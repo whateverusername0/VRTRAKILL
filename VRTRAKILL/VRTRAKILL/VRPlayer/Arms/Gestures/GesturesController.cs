c98ee83574cb9600f1e50470573ebca1a36d9465
whateverusername0 <whateveremail>
2023-06-12 15:01:25 +1000

    placeholders, people, placeholders!

---
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Gestures/GesturesController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.Arms.Gestures
+{
+    internal class GesturesController : MonoBehaviour
+    {
+        public Armature Arm;
+
+        public bool
+            Punch = false,
+            ThumbsUp = false,
+            Point = false,
+            MiddleFinger = false;
+
+        public void Start()
+        {
+            switch (Arm.Type)
+            {
+                case ArmType.Feedbacker:
+                    break;
+                case ArmType.Knuckleblaster:
+                    break;
+
+                case ArmType.Whiplash:
+                case ArmType.Spear:
+                default: Destroy(GetComponent<GesturesController>()); break;
+            }
+        }
+    }
+}
