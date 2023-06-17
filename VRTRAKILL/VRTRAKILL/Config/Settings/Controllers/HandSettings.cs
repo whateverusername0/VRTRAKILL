c7b960729106c2845ced552d8efcfaedbef007fc
whateverusername0 <whateveremail>
2023-06-18 02:09:39 +1000

    change name of a hand setting

--- VRTRAKILL/VRTRAKILL/Config/Settings/Controllers/HandSettings.cs
+++ VRTRAKILL/VRTRAKILL/Config/Settings/Controllers/HandSettings.cs
@@ -4,7 +4,7 @@ namespace Plugin.VRTRAKILL.Config.Settings.Controllers
 {
     public class HandSettings
     {
-        [JsonProperty("Swap Hands (Left-Handed Mode) (unused)")] public bool LeftHandMode { get; set; } = false;
+        [JsonProperty("Swap hands (left-handed mode)")] public bool LeftHandMode { get; set; } = false;
         [JsonProperty("Enable arms (VRIK) (unused)")] public bool EnableVRIK { get; set; } = false;
     }
 }
