c0a232661aff561a1c3c83a21c1d8edac6de5d85
whateverusername0 <whateveremail>
2023-06-29 03:12:57 +1000

    replace armature constructor w/ preset

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Patches/ArmsP.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/Patches/ArmsP.cs
@@ -6,24 +6,23 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms.Patches
     {
         [HarmonyPostfix] [HarmonyPatch(typeof(Punch), nameof(Punch.Start))] static void ConvertArms(Punch __instance)
         {
-            Armature A;
+            VRIK.Armature A = null;
             ArmRemover AR = __instance.gameObject.AddComponent<ArmRemover>();
             VRArmsController VRAC = __instance.gameObject.AddComponent<VRArmsController>();
             Gestures.GesturesController GC = __instance.gameObject.AddComponent<Gestures.GesturesController>();
-            VRIK.VRIKController VRIKC = __instance.gameObject.AddComponent<VRIK.VRIKController>();
             switch (__instance.type)
             {
                 case FistType.Standard:
-                    A = new Armature(__instance.transform, ArmType.Feedbacker);
-                    AR.Arm = A; VRAC.Arm = A; GC.Arm = A; VRIKC.Arm = A;
-                    break;
+                    A = VRIK.Armature.FeedbackerPreset(__instance.transform);
+                    Vars.FeedbackerArm = A; break;
                 case FistType.Heavy:
-                    A = new Armature(__instance.transform, ArmType.Knuckleblaster);
-                    AR.Arm = A; VRAC.Arm = A; GC.Arm = A; VRIKC.Arm = A;
-                    break;
+                    A = VRIK.Armature.KnuckleblasterPreset(__instance.transform);
+                    Vars.KnuckleblasterArm = A; break;
                 case FistType.Spear:
+                default:
                     break; // wtf is this?
             }
+            AR.Arm = A; VRAC.Arm = A; GC.Arm = A;
         }
     }
 }
