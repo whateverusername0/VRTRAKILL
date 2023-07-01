fcf389a34d46f03ceba9e68a3bff4414f71ef668
whateverusername0 <whateveremail>
2023-07-02 01:33:38 +1000

    implement something that looks like vrik

--- VRTRAKILL/VRTRAKILL/VRPlayer/Arms/VRArmsController.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/Arms/VRArmsController.cs
@@ -5,13 +5,19 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
 {
     internal class VRArmsController : MonoBehaviour
     {
-        public VRIK.Armature Arm; public bool UseVRIK;
-        public Vector3 OffsetPosition = new Vector3(.145f, .09f, .04f); // hack to fix whiplash
+        // basic stuff
+        public VRIK.Armature Arm;
+        public Vector3 OffsetPosition = new Vector3(.145f, .09f, .04f); // pre set to fix whiplash
         public Quaternion OffsetRotation = Quaternion.Euler(-90, 180, 0);
+        public bool IsSandboxer = false, IsRevolver = false;
 
+        // used for punching
         public Vector3 LastPosition, Velocity;
 
-        public bool IsSandboxer = false, IsRevolver = false;
+        // vrik stuff
+        public bool UseVRIK = false;
+        public Vector3 VRIKShoulderPosition = Vector3.zero;
+        public Vector3 VRIKArmScale = new Vector3(.325f, .325f, .325f);
 
         public void Start()
         {
@@ -32,8 +38,24 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
             }
 
             LastPosition = transform.position;
-        }
 
+            if (Vars.Config.Game.VRB.EnableVRIK)
+            {
+                UseVRIK = true;
+
+                VRIK.IKArm IKArm = Arm.Hand.gameObject.AddComponent<VRIK.IKArm>();
+                IKArm.ChainLength = 3;
+
+                if (IsRevolver) IKArm.Target = Vars.DominantHand.transform;
+                else IKArm.Target = Vars.NonDominantHand.transform;
+
+                GetComponent<ArmRemover>().enabled = false;
+            }
+        }
+        public void Update()
+        {
+            
+        }
         public void FixedUpdate()
         {
             if (LastPosition != transform.position)
@@ -44,43 +66,60 @@ namespace Plugin.VRTRAKILL.VRPlayer.Arms
         }
         public void LateUpdate()
         {
+            if (UseVRIK)
+            {
+                Arm.Root.localScale = VRIKArmScale;
+            }
+
             try
             {
                 // Update positions & rotations of the main gameobject + hand rotation (because animator stuff)
-                if (IsSandboxer)
-                {
-                    Arm.GameObjectT.position = Vars.DominantHand.transform.position;
-                    Arm.GameObjectT.rotation = Vars.DominantHand.transform.rotation;
-                    Arm.Root.localPosition = OffsetPosition;
-                    Arm.Hand.rotation = Vars.DominantHand.transform.rotation * OffsetRotation;
-                }
-                else if (IsRevolver)
-                {
-                    if (GetComponent<Revolver>().anim.GetBool("Spinning"))
-                        Arm.Hand.rotation = Vars.DominantHand.transform.rotation * OffsetRotation;
-                }
-                else
-                {
-                    Arm.GameObjectT.position = Vars.NonDominantHand.transform.position;
-                    Arm.GameObjectT.rotation = Vars.NonDominantHand.transform.rotation;
-                    Arm.Root.localPosition = OffsetPosition;
-                    Arm.Hand.rotation = Vars.NonDominantHand.transform.rotation * OffsetRotation;
-                }
+                if (IsSandboxer) MoveSandboxer();
+                else if (IsRevolver) HandleRevolver();
+                else MoveHand();
 
-                // Whiplash specific stuff
-                if (gameObject.HasComponent<HookArm>())
-                    Arm.Wrist.GetChild(1).rotation = Vars.NonDominantHand.transform.rotation * OffsetRotation;
+                HandleWhiplash();
 
-                // Thingamajig to disable other arms while grapplehooking
-                if (HookArm.Instance.model.activeSelf && !gameObject.HasComponent<HookArm>()
-                    && !IsSandboxer && !gameObject.HasComponent<Revolver>())
-                    Arm.GameObjectT.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
-                else Arm.GameObjectT.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
             } catch {} // do nothing because i know that it 100% works :) (it gives out too many errors which zipbomb your storage)
         }
-        private void MoveHands()
-        {
 
+        private void MoveSandboxer()
+        {
+            if (!UseVRIK)
+            {
+                Arm.GameObjectT.position = Vars.DominantHand.transform.position;
+                Arm.GameObjectT.rotation = Vars.DominantHand.transform.rotation;
+            }
+            Arm.Hand.position = Vars.DominantHand.transform.position;
+            Arm.Hand.rotation = Vars.DominantHand.transform.rotation * OffsetRotation;
+            Arm.Root.localPosition = OffsetPosition;
+        }
+        private void HandleRevolver()
+        {
+            if (GetComponent<Revolver>().anim.GetBool("Spinning"))
+                Arm.Hand.rotation = Vars.DominantHand.transform.rotation * OffsetRotation;
+        }
+        private void MoveHand()
+        {
+            if (!UseVRIK)
+            {
+                Arm.GameObjectT.position = Vars.NonDominantHand.transform.position;
+                Arm.GameObjectT.rotation = Vars.NonDominantHand.transform.rotation;
+            }
+            Arm.Root.localPosition = OffsetPosition;
+            Arm.Hand.position = Vars.NonDominantHand.transform.position;
+            Arm.Hand.rotation = Vars.NonDominantHand.transform.rotation * OffsetRotation;
+        }
+        private void HandleWhiplash()
+        {
+            if (gameObject.HasComponent<HookArm>())
+                Arm.Wrist.GetChild(1).rotation = Vars.NonDominantHand.transform.rotation * OffsetRotation;
+
+            // Thingamajig to disable other arms while grapplehooking
+            if (HookArm.Instance.model.activeSelf && !gameObject.HasComponent<HookArm>()
+                && !IsSandboxer && !gameObject.HasComponent<Revolver>())
+                Arm.GameObjectT.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+            else Arm.GameObjectT.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
         }
     }
 }
