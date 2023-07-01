709c793b1dd57017ad3785ed04970b2acac28eb3
whateverusername0 <whateveremail>
2023-07-02 00:52:39 +1000

    big ui rework

--- VRTRAKILL/VRTRAKILL/UI/Patches/CrosshairP.cs
+++ VRTRAKILL/VRTRAKILL/UI/Patches/CrosshairP.cs
@@ -14,15 +14,15 @@ namespace Plugin.VRTRAKILL.UI.Patches
             Canvas C = __instance.gameObject.AddComponent<Canvas>();
             C.worldCamera = Vars.UICamera;
             C.renderMode = RenderMode.WorldSpace;
-            __instance.gameObject.layer = (int)Vars.Layers.Default;
+            __instance.gameObject.layer = (int)Vars.Layers.UI;
 
-            if (__instance.gameObject.HasComponent<VRUIController>())
-                Object.Destroy(__instance.gameObject.GetComponent<VRUIController>());
+            if (__instance.gameObject.HasComponent<UICanvas>())
+                Object.Destroy(__instance.gameObject.GetComponent<UICanvas>());
 
             __instance.transform.localScale /= 2; // to fit in player's size (shitcode, but it works!!)
             __instance.transform.localPosition += new Vector3(0, 0, .05f);
             __instance.transform.localEulerAngles = Vector3.zero;
-            __instance.gameObject.AddComponent<CrosshairController>();
+            //__instance.gameObject.AddComponent<CrosshairController>();
         }
     }
 }
---
+++ VRTRAKILL/VRTRAKILL/UI/UIConverter.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Plugin.VRTRAKILL.UI
+{
+    // mostly "borrowed" from huskvr
+    internal class UIConverter
+    {
+        public static Camera UICamera { get; private set; }
+
+        public static void Init()
+        => SceneManager.activeSceneChanged += (x, y) => SceneChanged(y);
+
+        private static void SceneChanged(Scene S)
+        {
+            UICamera = new GameObject("UI Camera").AddComponent<Camera>();
+            UICamera.cullingMask = 1 << (int)Vars.Layers.UI;
+            UICamera.clearFlags = CameraClearFlags.Depth; UICamera.depth = 1;
+
+            if (!Vars.Config.Controllers.UseControllerUIInteraction)
+                UICamera.gameObject.AddComponent<UIInteraction>();
+
+            foreach (Canvas C in Object.FindObjectsOfType<Canvas>())
+                if (!Helpers.Misc.HasComponent<UICanvas>(C.gameObject))
+                    RecursiveConvertCanvas();
+        }
+
+        public static void RecursiveConvertCanvas(GameObject GO = null)
+        {
+            if (GO != null)
+            {
+                try { ConvertCanvas(GO.GetComponent<Canvas>()); } catch {}
+
+                if (GO.transform.childCount > 0)
+                    for (int i = 0; i < GO.transform.childCount; i++)
+                        RecursiveConvertCanvas(GO.transform.GetChild(i).gameObject);
+            }
+            else
+            {
+                foreach (Canvas C in Object.FindObjectsOfType<Canvas>())
+                    if (!Helpers.Misc.HasComponent<UICanvas>(C.gameObject))
+                        try { ConvertCanvas(C); } catch {}
+            }
+        }
+        public static void ConvertCanvas(Canvas C, bool Force = false, bool DontAddComponent = false)
+        {
+            if (!Force)
+            {
+                if (C.renderMode != RenderMode.ScreenSpaceOverlay) return;
+            }
+
+            C.worldCamera = UICamera;
+            C.renderMode = RenderMode.WorldSpace;
+            C.gameObject.layer = (int)Vars.Layers.UI;
+            if (!DontAddComponent) C.gameObject.AddComponent<UICanvas>();
+
+            foreach (Transform Child in C.transform) ConvertElement(Child);
+        }
+        private static void ConvertElement(Transform Element)
+        {
+            if (Element.GetComponent<Selectable>() is Selectable Button)
+            {
+                ColorBlock block = Button.colors;
+                block.highlightedColor = Color.red;
+                Button.colors = block;
+            }
+
+            foreach (Transform Child in Element) ConvertElement(Child);
+        }
+    }
+}
