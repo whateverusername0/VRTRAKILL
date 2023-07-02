17063656b3fa20dac045bbdcebda2d2e0a47a679
whateverusername0 <whateveremail>
2023-07-02 19:54:56 +1000

    rename Shin to Calf

--- VRTRAKILL/VRTRAKILL/VRPlayer/VRIK/Leggature.cs
+++ VRTRAKILL/VRTRAKILL/VRPlayer/VRIK/Leggature.cs
@@ -6,7 +6,7 @@ namespace Plugin.VRTRAKILL.VRPlayer.VRIK
     {
         public Transform GameObjectT { get; set; }
         public Transform Thigh { get; set; }
-        public Transform Shin { get; set; }
+        public Transform Calf { get; set; }
         public Transform Foot { get; set; }
         public Transform Heel { get; set; }
         public Transform Toe { get; set; }
@@ -16,8 +16,8 @@ namespace Plugin.VRTRAKILL.VRPlayer.VRIK
             Leggature L = new Leggature();
             L.GameObjectT = T;
             L.Thigh = L.GameObjectT;
-            L.Shin = L.Thigh.GetChild(0);
-            L.Foot = L.Shin.GetChild(0);
+            L.Calf = L.Thigh.GetChild(0);
+            L.Foot = L.Calf.GetChild(0);
             L.Heel = L.Foot.GetChild(0);
             L.Toe = L.Foot.GetChild(1);
             return L;
