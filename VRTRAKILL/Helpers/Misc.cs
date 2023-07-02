21e63d55cfb5f6d9407c713fe53ce503f056ce10
whateverusername0 <whateveremail>
2023-07-02 19:52:23 +1000

    add forcefindgameobject method

--- VRTRAKILL/Helpers/Misc.cs
+++ VRTRAKILL/Helpers/Misc.cs
@@ -7,6 +7,13 @@ namespace Plugin.Helpers
         public static bool HasComponent<T>(this GameObject GM) where T : Component
         { return GM.GetComponent<T>() != null; }
 
+        public static GameObject ForceFindGameObject(string Name)
+        {
+            foreach (GameObject GO in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                if (GO.name == Name) return GO;
+            return null;
+        }
+
         public static void RecursiveChangeLayer(this GameObject GM, LayerMask LM)
         {
             GM.layer = LM;
