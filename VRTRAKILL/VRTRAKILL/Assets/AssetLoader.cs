3593905d5cf9543f4ad2acd16502b38e87d87f53
whateverusername0 <whateveremail>
2023-07-09 03:42:40 +1000

    updated assetbundle

--- VRTRAKILL/VRTRAKILL/Assets/AssetLoader.cs
+++ VRTRAKILL/VRTRAKILL/Assets/AssetLoader.cs
@@ -3,20 +3,29 @@
 namespace Plugin.VRTRAKILL.Assets
 {
     // check manifests in VRTRAKILL/AssetBundles to know which one is which
-
     internal class AssetLoader
     {
-        public static GameObject V1Rig;
+        public static GameObject V1Rig, V2Rig;
+        public static GameObject HandPose_Shotgun,
+                                 HandPose_Nailgun,
+                                 HandPose_Sawblade,
+                                 HandPose_Railgun;
 
-        public static void LoadCustomAssets()
+        public static void LoadAllCustomAssets()
         {
-            AssetBundle Assets = LoadBundle("Rigs\\mdl_v1rigged");
-            V1Rig = Object.Instantiate(LoadAsset<GameObject>(Assets, "v1/V1Rig.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
-            
+            AssetBundle Assets = LoadBundle("vrtrakillassetbundle");
+            V1Rig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            V2Rig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V2/V2.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+
+            HandPose_Shotgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Shotgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            HandPose_Nailgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Nailgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            HandPose_Sawblade = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Sawblade.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            HandPose_Railgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Railgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+
             Assets.Unload(false);
         }
 
-        public static T LoadAsset<T>(AssetBundle Bundle, string PrefabName, string PathToPrefab = "Assets/ASSets/things") where T : Object
+        public static T LoadAsset<T>(AssetBundle Bundle, string PrefabName, string PathToPrefab = "Assets/AssetsBundles") where T : Object
         {
             var Asset = Bundle.LoadAsset<T>($"{PathToPrefab}/{PrefabName}");
             if (Asset != null) return Asset;
@@ -24,7 +33,7 @@ namespace Plugin.VRTRAKILL.Assets
         }
         public static AssetBundle LoadBundle(string BundleName, string Path = null)
         {
-            if (Path == null) Path = $"{Plugin.AssetsPath}\\";
+            if (Path == null) Path = $"{Plugin.PluginPath}\\";
             return AssetBundle.LoadFromFile($"{Path}\\{BundleName}");
         }
     }
--- VRTRAKILL/VRTRAKILL/Assets/Vars.cs
+++ VRTRAKILL/VRTRAKILL/Assets/Vars.cs
@@ -5,5 +5,11 @@ namespace Plugin.VRTRAKILL.Assets
     static class Vars
     {
         public static GameObject V1Rig => AssetLoader.V1Rig;
+        public static GameObject V2Rig => AssetLoader.V2Rig;
+
+        public static GameObject HandPose_Shotgun => AssetLoader.HandPose_Shotgun;
+        public static GameObject HandPose_Nailgun => AssetLoader.HandPose_Nailgun;
+        public static GameObject HandPose_Sawblade => AssetLoader.HandPose_Sawblade;
+        public static GameObject HandPose_Railgun => AssetLoader.HandPose_Railgun;
     }
 }
