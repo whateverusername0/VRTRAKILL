eb561b291520218b5a8799457b985a8945e80d67
whateverusername0 <whateveremail>
2023-07-09 16:33:24 +1000

    shit

---
+++ New Unity Project/Assets/OtherAssets/things/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Transform P1;
+    public Transform P2;
+
+    void Update()
+    {
+        transform.forward = P2.position - P1.position;
+    }
+}
