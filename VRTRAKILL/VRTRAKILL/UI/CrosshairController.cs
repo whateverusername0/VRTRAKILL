12ba75f8054c26f3e3f41d1c2111248b6a013df8
whateverusername0 <whateveremail>
2023-07-14 14:25:48 +1000

    hopefully fixed crosshair pos

--- VRTRAKILL/VRTRAKILL/UI/CrosshairController.cs
+++ VRTRAKILL/VRTRAKILL/UI/CrosshairController.cs
@@ -7,9 +7,11 @@ namespace Plugin.VRTRAKILL.UI
     {
         public float DefaultLength => Vars.Config.View.VRUI.CrosshairDistance;
 
+        Vector3 Offset = new Vector3(-.2f, -2.75f, 0);
+
         public void LateUpdate()
         {
-            transform.localPosition = new Vector3(-.2f, -2.75f, Vars.Config.View.VRUI.CrosshairDistance);
+            transform.position = transform.parent.position + (transform.parent.forward * DefaultLength) + Offset;
         }
     }
 }
