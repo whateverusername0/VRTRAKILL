00f2c53c9fb70dacae3a9f87012a607ba316d6a6
whateverusername0 <whateveremail>
2023-07-15 21:44:00 +1000

    added relayering hand poses

--- VRTRAKILL/VRTRAKILL/SceneWorker.cs
+++ VRTRAKILL/VRTRAKILL/SceneWorker.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Reflection;
+using Plugin.Helpers;
 
 namespace Plugin.VRTRAKILL
 {
@@ -9,6 +11,8 @@ namespace Plugin.VRTRAKILL
         public static void Init()
         => SceneManager.activeSceneChanged += (x, y) => SceneChanged(y);
 
+        // cool message suppersion :)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
         private static void SceneChanged(Scene S)
         {
             GloballyEnableOffscreenRendering();
@@ -16,12 +20,28 @@ namespace Plugin.VRTRAKILL
             Config.ConfigJSON.Instance = null; // reload config
             UI.UIConverter.ConvertAllCanvases();
             Assets.AssetLoader.LoadAllCustomAssets();
+
+            RelayerAssetsStuff();
         }
 
         private static void GloballyEnableOffscreenRendering()
         {
             foreach (SkinnedMeshRenderer R in Object.FindObjectsOfType<SkinnedMeshRenderer>())
                 R.updateWhenOffscreen = true;
+        }
+        private static void RelayerAssetsStuff()
+        {
+            // thanks to unity being so fucking weird instead of actual gameobjects it stores references to them,
+            // so this works.
+            foreach (PropertyInfo I in typeof(Assets.Vars).GetProperties())
+            {
+                if (I.GetValue(typeof(Assets.Vars), null) != null)
+                {
+                    GameObject GO = (GameObject)I.GetValue(typeof(Assets.Vars), null);
+                    GO.RecursiveChangeLayer((int)Vars.Layers.AlwaysOnTop);
+                }
+                else continue;
+            }
         }
     }
 }
