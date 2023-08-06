fded59f07e36f252051c52e274684240a99282f2
whateverusername0 <whateveremail>
2023-08-07 04:30:09 +1000

    more improvements. skins support

--- Plugin/VRTRAKILL/Assets/AssetLoader.cs
+++ Plugin/VRTRAKILL/Assets/AssetLoader.cs
@@ -5,22 +5,43 @@ namespace Plugin.VRTRAKILL.Assets
     // check manifests in VRTRAKILL/AssetBundles to know which one is which
     internal class AssetLoader
     {
-        public static GameObject V1Rig, V2Rig;
-        public static GameObject HandPose_Shotgun,
-                                 HandPose_Nailgun,
-                                 HandPose_Sawblade,
-                                 HandPose_Railgun;
+        // V1 Rig (also usable for V2)
+        public static GameObject VRig { get; private set; }
+
+        // 0 - Body, 1 - Wing, 2 - Hand
+        public static Material[] V1Skin { get; private set; }
+        public static Material[] V2Skin { get; private set; }
+
+        // Hand Poses
+        public static GameObject HandPose_Shotgun { get; private set; }
+        public static GameObject HandPose_Nailgun{ get; private set; }
+        public static GameObject HandPose_Sawblade{ get; private set; }
+        public static GameObject HandPose_Railgun { get; private set; }
 
         public static void LoadAllCustomAssets()
         {
             AssetBundle Assets = LoadBundle("vrtrakillassetbundle");
-            V1Rig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
-            V2Rig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V2/V2.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            VRig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
 
             HandPose_Shotgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Shotgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Nailgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Nailgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Sawblade = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Sawblade.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Railgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Railgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+
+            if (V1Skin == null)
+                V1Skin = new Material[]
+                {
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Body.mat")),
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Wing.mat")),
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Arms.mat"))
+                };
+            if (V2Skin == null)
+                V2Skin = new Material[]
+                {
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Body.mat")),
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Wing.mat")),
+                    Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Arms.mat"))
+                };
 
             Assets.Unload(false);
         }
