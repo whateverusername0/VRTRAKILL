3d4febdda72728d5d46e87c786c606ef4a1d5086
whateverusername0 <whateveremail>
2023-08-08 04:35:26 +1000

    revamped positions, added reverse ones

--- Plugin/VRTRAKILL/UI/Patches/HudP.cs
+++ Plugin/VRTRAKILL/UI/Patches/HudP.cs
@@ -5,10 +5,18 @@ namespace Plugin.VRTRAKILL.UI.Patches
 {
     [HarmonyPatch] internal class HudP
     {
-        [HarmonyPrefix] [HarmonyPatch(typeof(HUDPos), nameof(HUDPos.Start))] static void ReparentHUD(HUDPos __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(HudController), nameof(HudController.Start))] static void ReparentHUD(HudController __instance)
         {
-            __instance.transform.parent = HUDOptions.Instance.transform;
-
+            if (__instance.altHud) return;
+            else
+            {
+                __instance.transform.parent = CanvasController.Instance.transform;
+                __instance.transform.localPosition = Vector3.zero;
+                __instance.transform.localEulerAngles = Vector3.zero;
+            }
+        }
+        [HarmonyPrefix] [HarmonyPatch(typeof(HUDPos), nameof(HUDPos.Start))] static void RetransformElements(HUDPos __instance)
+        {
             UIConverter.ConvertCanvas(__instance.GetComponent<Canvas>(), Force: true, DontAddComponent: true);
             __instance.gameObject.AddComponent<HideWhenMenuActive>();
             
@@ -18,27 +26,28 @@ namespace Plugin.VRTRAKILL.UI.Patches
                     StandardHUDWorker SHUDW = __instance.transform.parent.gameObject.AddComponent<StandardHUDWorker>();
                     SHUDW.StandardHUD = __instance.gameObject;
 
-                    __instance.defaultPos = new Vector3(-520, -240, 460); __instance.transform.localPosition = new Vector3(-520, -240, 460);
-                    __instance.defaultRot = new Vector3(355, 315, 1.5f); __instance.transform.localEulerAngles = new Vector3(355, 315, 1.5f);
-                    // reversepos
-                    // reverserot
-                    
+                    __instance.transform.localScale = new Vector3(.25f, .25f, .25f);
+                    __instance.defaultPos = new Vector3(-360, -220, -240);
+                    __instance.defaultRot = new Vector3(0, -45, 0);
+
+                    __instance.reversePos = new Vector3(360, -220, -240);
+                    __instance.reverseRot = new Vector3(0, 45, 0);
                     break;
+
                 case "StyleCanvas":
-                    __instance.defaultPos = new Vector3(324, 48, -286); __instance.transform.localPosition = new Vector3(324, 48, -286);
-                    __instance.defaultRot = new Vector3(0, 56, 0); __instance.transform.localEulerAngles = new Vector3(0, 56, 0);
-                    // reversepos
-                    // reverserot
+                    __instance.defaultPos = new Vector3(320, 60, -240);
+                    __instance.defaultRot = new Vector3(356, 65, 357);
+
+                    __instance.reversePos = new Vector3(-320, 60, -240);
+                    __instance.reverseRot = new Vector3(0, -65, 0);
                     break;
             }
             __instance.transform.localScale = new Vector3(.5f, .5f, .5f);
         }
-        [HarmonyPostfix] [HarmonyPatch(typeof(FinalRank), nameof(FinalRank.Start))] static void ReparentFR(FinalRank __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(FinalRank), nameof(FinalRank.Start))] static void ConvertFinalRank(FinalRank __instance)
         {
-            __instance.transform.parent.transform.parent = HUDOptions.Instance.transform;
-
             UIConverter.ConvertCanvas(__instance.transform.parent.GetComponent<Canvas>(), Force: true);
-
+            __instance.transform.localPosition = Vector3.zero;
             __instance.transform.localScale = new Vector3(16, 16, 16);
         }
     }
