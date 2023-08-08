1b3d4cc5db2a9a1adce858d576c11ca44baaf93c
whateverusername0 <whateveremail>
2023-08-08 23:27:02 +1000

    hud offsets tweak

--- Plugin/VRTRAKILL/UI/Patches/HudP.cs
+++ Plugin/VRTRAKILL/UI/Patches/HudP.cs
@@ -27,19 +27,19 @@ namespace Plugin.VRTRAKILL.UI.Patches
                     SHUDW.StandardHUD = __instance.gameObject;
 
                     __instance.transform.localScale = new Vector3(.25f, .25f, .25f);
-                    __instance.defaultPos = new Vector3(-360, -220, -240);
-                    __instance.defaultRot = new Vector3(0, -45, 0);
+                    __instance.defaultPos = new Vector3(-640, -220, 120);
+                    __instance.defaultRot = new Vector3(0, -20, 0);
 
-                    __instance.reversePos = new Vector3(360, -220, -240);
-                    __instance.reverseRot = new Vector3(0, 45, 0);
+                    __instance.reversePos = new Vector3(640, -220, 120);
+                    __instance.reverseRot = new Vector3(0, 20, 0);
                     break;
 
                 case "StyleCanvas":
-                    __instance.defaultPos = new Vector3(320, 60, -240);
-                    __instance.defaultRot = new Vector3(356, 65, 357);
+                    __instance.defaultPos = new Vector3(360, 60, -240);
+                    __instance.defaultRot = new Vector3(356, 45, 357);
 
-                    __instance.reversePos = new Vector3(-320, 60, -240);
-                    __instance.reverseRot = new Vector3(0, -65, 0);
+                    __instance.reversePos = new Vector3(-360, 60, -240);
+                    __instance.reverseRot = new Vector3(0, -45, 0);
                     break;
             }
             __instance.transform.localScale = new Vector3(.5f, .5f, .5f);
