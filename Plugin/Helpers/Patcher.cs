013c1d0c0c738c6ee34e426c1762d654722c3f29
whateverusername0 <whateveremail>
2023-08-08 18:26:25 +1000

    Patcher.cs: fixed a critical bug

--- Plugin/Helpers/Patcher.cs
+++ Plugin/Helpers/Patcher.cs
@@ -15,9 +15,8 @@ namespace Plugin.Helpers
         public Harmony Harmony { get; set; }
         public Assembly ASS { get; private set; } = Assembly.GetCallingAssembly();
         public string Namespace { get; set; } public Type Type { get; set; }
-        public string[] Namespaces { get; set; } public Type[] Types { get; set; }
+        public string[] Namespaces { get; set; } public List<Type> Types { get; set; } = new List<Type>();
 
-        // That's a LOT of constructors that i'm not so proud of
         public Patcher(Harmony _Harmony)
         {
             Harmony = _Harmony;
@@ -40,7 +39,7 @@ namespace Plugin.Helpers
         public Patcher(Harmony _Harmony, Type[] _Types)
         {
             Harmony = _Harmony;
-            Types = _Types;
+            Types = _Types.ToList();
         }
         public Patcher(Harmony _Harmony, Assembly _ASS)
         {
@@ -69,7 +68,7 @@ namespace Plugin.Helpers
         {
             Harmony = _Harmony;
             ASS = _ASS;
-            Types = _Types;
+            Types = _Types.ToList();
         }
 
         private List<Type> GetPatches(string _Namespace = null)
@@ -96,16 +95,28 @@ namespace Plugin.Helpers
                 QL.AddRange(GetPatches(_Namespace));
             return QL;
         }
+        public IEnumerable<Type> GetTypes(string _Namespace)
+        { return GetPatches(_Namespace); }
+        public IEnumerable<Type> GetTypes(string[] _Namespaces)
+        { return GetPatches(_Namespaces); }
 
         public void PatchAll()
         {
-            // this looks like something YandereDev would do but it works
-            if (Namespace != null) PatchAll(Namespace);
-            else if (Namespaces != null && Namespaces.Length > 0) PatchAll(Namespaces);
-            else if (Type != null) PatchAll(Type);
-            else if (Types != null && Types.Length > 0) PatchAll(Types);
-            else Harmony.PatchAll();
+            List<Type> L = new List<Type>();
+
+            if (Namespace == null && Namespaces == null && Type == null && Types == null)
+                try { Harmony.PatchAll(); } catch (NullReferenceException) { Plugin.PLog.LogFatal("Could not find any patches(???) wtf?"); }
+            else
+            {
+                if (Namespace != null) L.AddRange(GetTypes(Namespace));
+                if (Namespaces != null && Namespaces.Length > 0) L.AddRange(GetTypes(Namespaces));
+                if (Type != null) L.Add(Type);
+                if (Types != null && Types.Count > 0) L.AddRange(Types.ToArray());
+                PatchAll(L.ToArray());
+            }
         }
+        
+
         public void PatchAll(string _Namespace)
         {
             IEnumerable<Type> Q = GetPatches(_Namespace);
