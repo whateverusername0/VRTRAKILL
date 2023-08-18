e994ef5101e6ce87ed7100fa792aea9d61f0e7fd
whateverusername0 <whateveremail>
2023-08-19 02:49:30 +1000

    assetload: updated assetbundle

--- Plugin/VRTRAKILL/Assets/AssetLoader.cs
+++ Plugin/VRTRAKILL/Assets/AssetLoader.cs
@@ -7,11 +7,7 @@ namespace Plugin.VRTRAKILL.Assets
     {
         // V1 Rig (also usable for V2)
         public static GameObject VRig { get; private set; }
-        //public static GameObject VHead { get; private set; }
-
-        // 0 - Body, 1 - Wing, 2 - Hand
-        public static Material[] V1Skin { get; private set; }
-        public static Material[] V2Skin { get; private set; }
+        public static GameObject VHead { get; private set; }
 
         // Hand Poses
         public static GameObject HandPose_Shotgun { get; private set; }
@@ -23,27 +19,12 @@ namespace Plugin.VRTRAKILL.Assets
         {
             AssetBundle Assets = LoadBundle("vrtrakillassetbundle");
             VRig = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
-            //VHead = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1_Head.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
+            VHead = Object.Instantiate(LoadAsset<GameObject>(Assets, "V1/V1_Head.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
 
             HandPose_Shotgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Shotgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Nailgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Nailgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Sawblade = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Sawblade.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
             HandPose_Railgun = Object.Instantiate(LoadAsset<GameObject>(Assets, "Arms/Feedbacker/Hand_Railgun.prefab"), new Vector3(2048, 2048, 2048), Quaternion.identity);
-
-            //if (V1Skin == null)
-            //    V1Skin = new Material[]
-            //    {
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Body.mat")),
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Wing.mat")),
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V1/Mat_Arms.mat"))
-            //    };
-            //if (V2Skin == null)
-            //    V2Skin = new Material[]
-            //    {
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Body.mat")),
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Wing.mat")),
-            //        Object.Instantiate(LoadAsset<Material>(Assets, "V2/Mat_Arms.mat"))
-            //    };
 
             Assets.Unload(false);
         }
--- Plugin/VRTRAKILL/Assets/Vars.cs
+++ Plugin/VRTRAKILL/Assets/Vars.cs
@@ -5,10 +5,7 @@ namespace Plugin.VRTRAKILL.Assets
     public static class Vars
     {
         public static GameObject VRig => AssetLoader.VRig;
-        //public static GameObject VHead => AssetLoader.VHead;
-
-        public static Material[] Skin_V1 => AssetLoader.V1Skin;
-        public static Material[] Skin_V2 => AssetLoader.V2Skin;
+        public static GameObject VHead => AssetLoader.VHead;
 
         public static GameObject HandPose_Shotgun => AssetLoader.HandPose_Shotgun;
         public static GameObject HandPose_Nailgun => AssetLoader.HandPose_Nailgun;
