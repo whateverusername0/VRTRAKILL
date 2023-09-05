6c65d3ae5993bc445e78318e856cd71d44313ec1
whateverusername0 <whateveremail>
2023-09-06 00:47:38 +1000

    formatting, csproj

--- Plugin/VRTRAKILL/VRPlayer/Guns/Patches/RemoveArmsP.cs
+++ Plugin/VRTRAKILL/VRPlayer/Guns/Patches/RemoveArmsP.cs
@@ -9,12 +9,12 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
     {
         [HarmonyPostfix] [HarmonyPatch(typeof(Revolver), nameof(Revolver.Start))] static void RemoveRevolverArm(Revolver __instance)
         {
-            try { __instance.gameObject.AddComponent<Arms.ArmRemover>(); }
+            try { __instance.gameObject.AddComponent<Arms.ArmTransformer>(); }
             catch (NullReferenceException) { Vars.Log.LogWarning($"Revolver is null???"); }
         }
         [HarmonyPostfix] [HarmonyPatch(typeof(SandboxArm), nameof(SandboxArm.Awake))] static void RemoveSandboxArm(SandboxArm __instance)
         {
-            try { __instance.gameObject.AddComponent<Arms.ArmRemover>(); }
+            try { __instance.gameObject.AddComponent<Arms.ArmTransformer>(); }
             catch (NullReferenceException) { Vars.Log.LogWarning($"Sandbox arm is null???"); }
         }
         [HarmonyPostfix] [HarmonyPatch(typeof(Shotgun), nameof(Shotgun.Start))] static void RemoveShotgunArm(Shotgun __instance)
