775acbab2673f1ce81842d8cbf5c02db3828723b
whateverusername0 <whateveremail>
2023-09-05 17:34:45 +1000

    critical bugfix

--- Plugin/VRTRAKILL/UI/Patches/CrosshairP.cs
+++ Plugin/VRTRAKILL/UI/Patches/CrosshairP.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 namespace Plugin.VRTRAKILL.UI.Patches
 {
-    [HarmonyPatch(typeof(Crosshair))] internal sealed class CrosshairP
+    [HarmonyPatch] internal class CrosshairP
     {
-        [HarmonyPrefix] [HarmonyPatch(nameof(Crosshair.Start))] static void SetCrosshair(Crosshair __instance)
+        [HarmonyPrefix] [HarmonyPatch(typeof(Crosshair), nameof(Crosshair.Start))] static void SetCrosshair(Crosshair __instance)
         {
             Canvas C = __instance.gameObject.AddComponent<Canvas>();
             UIConverter.ConvertCanvas(C, DontAddComponent: true);
@@ -15,9 +15,23 @@ namespace Plugin.VRTRAKILL.UI.Patches
             __instance.transform.localEulerAngles = Vector3.zero;
             __instance.gameObject.AddComponent<CrosshairController>();
         }
-        [HarmonyPostfix] [HarmonyPatch(nameof(Crosshair.Start))] static void Reparent(Crosshair __instance)
+        [HarmonyPostfix] [HarmonyPatch(typeof(Crosshair), nameof(Crosshair.Start))] static void Reparent(Crosshair __instance)
         {
             __instance.transform.parent = null;
+        }
+        [HarmonyPrefix] [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.HideShit))] static bool FixShit(StatsManager __instance)
+        {
+            if (__instance.shud == null) __instance.shud = MonoSingleton<StyleHUD>.Instance;
+
+            __instance.shud.ComboOver();
+            if (__instance.gunc == null) __instance.gunc = MonoSingleton<GunControl>.Instance;
+
+            __instance.gunc.NoWeapon();
+            if ((bool)__instance.crosshair)
+                __instance.crosshair.transform.gameObject.SetActive(value: false);
+
+            HudController.Instance.gunCanvas.SetActive(value: false);
+            return false;
         }
     }
 }
