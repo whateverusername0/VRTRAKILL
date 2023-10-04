922c95b77b59a2a1be10dfdd3afd336369d3bed0
whateverusername0 <whateveremail>
2023-10-04 18:31:55 +1000

    Plugin: added AvatarSizeCalibrator placeholder

---
+++ Plugin/VRTRAKILL/VRPlayer/VRAvatar/AvatarSizeCalibrator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Plugin.VRTRAKILL.VRPlayer.VRAvatar
+{
+    [RequireComponent(typeof(VRigController))]
+    internal class AvatarSizeCalibrator : MonoBehaviour
+    {
+        private static AvatarSizeCalibrator _Instance; public static AvatarSizeCalibrator Instance { get { return _Instance; } }
+        public MetaRig Rig => VRigController.Instance.Rig;
+
+        public void Awake()
+        {
+            if (_Instance != null && _Instance != this) Destroy(this.gameObject);
+            else _Instance = this;
+        }
+
+
+    }
+}
