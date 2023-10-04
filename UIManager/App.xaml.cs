8063347bda6b531c05725ada2c5bbeb3f27a7004
whateverusername0 <whateveremail>
2023-10-04 18:31:40 +1000

    UIManager: added a lot of placeholders

---
+++ UIManager/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace UIManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly string APPLICATIONPATH = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static Model.Config Config => Model.Config.GetInstance().Instance;
+
+        public static Dictionary<string, ResourceDictionary> Themes = new Dictionary<string, ResourceDictionary>()
+        {
+            { "Light", null },
+            { "Dark", null },
+            { "ULTRAKILL", null }
+        };
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+
+            new View.Windows.MainWindow().Show();
+            View.Windows.MainWindow.Instance.MainFrame.Navigate(new View.Pages.Startup());
+        }
+    }
+}
