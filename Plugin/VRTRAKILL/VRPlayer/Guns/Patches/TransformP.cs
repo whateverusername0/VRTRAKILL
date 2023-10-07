6531c090add46baef212bef388aebcd8ae9c796f
whateverusername0 <whateveremail>
2023-10-07 13:36:15 +1000

    updated TransformP, allowing easier expansion

--- Plugin/VRTRAKILL/VRPlayer/Guns/Patches/TransformP.cs
+++ Plugin/VRTRAKILL/VRPlayer/Guns/Patches/TransformP.cs
@@ -6,56 +6,61 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
 {
     [HarmonyPatch] internal class TransformP
     {
-        // Applies offset to guns' transforms
+        /* Template for transform values:
+         static Vector3 Position = new Vector3(),
+                        Rotation = new Vector3(),
+                        Scale = new Vector3(),
+                        AltPosition = new Vector3(),
+                        AltRotation = new Vector3(),
+                        AltScale = new Vector3();
+         */
         [HarmonyPatch(typeof(Revolver))] static class RevolverT
         {
             static Vector3 Position = new Vector3(.05f, -.1f, .6f),
-                           AltPosition = new Vector3(.05f, -.075f, .5f),
+                           Rotation = new Vector3(),
                            Scale = new Vector3(.1f, .1f, .1f),
+                           AltPosition = new Vector3(.05f, -.075f, .5f),
+                           AltRotation = new Vector3(),
                            AltScale = new Vector3(.085f, .085f, .085f),
-                           OffsetRotation = new Vector3(0, -90, -90);
+                           HandOffsetRotation = new Vector3(0, -90, -90);
 
             [HarmonyPostfix] [HarmonyPatch(nameof(Revolver.Start))] static void Retransform(Revolver __instance)
             {
                 Arms.ArmController.WeaponArmCon WAC = __instance.gameObject.AddComponent<Arms.ArmController.WeaponArmCon>();
                 Arm A = Arm.FeedbackerPreset(__instance.transform);
-                WAC.SetArm(A); WAC.OffsetRot = OffsetRotation;
+                WAC.SetArm(A); WAC.OffsetRot = HandOffsetRotation;
 
-                if (__instance.altVersion) { __instance.wpos.defaultPos = AltPosition; __instance.wpos.defaultScale = AltScale; }
-                else { __instance.wpos.defaultPos = Position; __instance.wpos.defaultScale = Scale; }
+                if (__instance.altVersion) { ApplyTransform(ref __instance.wpos, AltPosition, AltRotation, AltScale); }
+                else { ApplyTransform(ref __instance.wpos, Position, Rotation, Scale); }
             }
         }
         [HarmonyPatch(typeof(Shotgun))] static class ShotgunT
         {
-            static Vector3 Position = new Vector3(-.02f, .2f, .26f);
-            static Vector3 Scale = new Vector3(.1f, .1f, .1f);
+            static Vector3 Position = new Vector3(-.02f, .2f, .26f),
+                           Rotation = new Vector3(),
+                           Scale = new Vector3(.1f, .1f, .1f),
+                           AltPosition = new Vector3(),
+                           AltRotation = new Vector3(),
+                           AltScale = new Vector3();
 
             [HarmonyPostfix] [HarmonyPatch(nameof(Shotgun.Start))] static void Retransform(Shotgun __instance)
             {
-                __instance.wpos.defaultPos = Position;
-                __instance.wpos.defaultScale = Scale;
+                ApplyTransform(ref __instance.wpos, Position, Rotation, Scale);
             }
         }
         [HarmonyPatch(typeof(Nailgun))] static class NailgunT
         {
             static Vector3 Position = new Vector3(-.165f, .1f, .045f),
-                           AltPosition = new Vector3(-.165f, .2f, .065f);
-            static Vector3 Scale = new Vector3(.4f, .3275f, .4f),
+                           Rotation = new Vector3(),
+                           Scale = new Vector3(.4f, .3275f, .4f),
+                           AltPosition = new Vector3(-.165f, .2f, .065f),
+                           AltRotation = new Vector3(),
                            AltScale = new Vector3(.35f, .35f, .35f);
 
             [HarmonyPostfix] [HarmonyPatch(nameof(Nailgun.Start))] static void Retransform(Nailgun __instance)
             {
-                if (__instance.altVersion)
-                {
-                    __instance.wpos.defaultPos = AltPosition;
-                    __instance.wpos.defaultScale = AltScale;
-                }
-                else
-                {
-                    __instance.wpos.defaultPos = Position;
-                    __instance.wpos.defaultScale = Scale;
-                }
-                
+                if (__instance.altVersion) { ApplyTransform(ref __instance.wpos, AltPosition, AltRotation, AltScale); }
+                else { ApplyTransform(ref __instance.wpos, Position, Rotation, Scale); }
             }
         }
         [HarmonyPatch(typeof(Railcannon))] static class RailcannonT
@@ -65,20 +70,22 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
 
             [HarmonyPostfix] [HarmonyPatch(nameof(Railcannon.Start))] static void Retransform(Railcannon __instance)
             {
-                __instance.wpos.defaultPos += Position;
+                __instance.wpos.defaultPos += Position; // what and why?
                 __instance.wpos.defaultScale = Scale;
             }
         }
         [HarmonyPatch(typeof(RocketLauncher))] static class RocketLauncherT
         {
-            static Vector3 Position = new Vector3(-.3f, .3f, -.1f);
-            static Vector3 Scale = new Vector3(.65f, .65f, .65f);
+            static Vector3 Position = new Vector3(-.3f, .3f, -.1f),
+                           Rotation = new Vector3(),
+                           Scale = new Vector3(.65f, .65f, .65f),
+                           AltPosition = new Vector3(),
+                           AltRotation = new Vector3(),
+                           AltScale = new Vector3();
 
             [HarmonyPostfix] [HarmonyPatch(nameof(RocketLauncher.Start))] static void Retransform(RocketLauncher __instance)
             {
-                __instance.gameObject.GetComponent<WeaponPos>().defaultPos = Position;
-                __instance.gameObject.GetComponent<WeaponPos>().defaultScale = Scale;
-
+                ApplyTransform(__instance.GetComponent<WeaponPos>(), Position, Rotation, Scale);
                 if (VRAvatar.VRigController.Instance != null)
                     __instance.transform.GetChild(0).localPosition = Vector3.zero;
             }
@@ -100,15 +107,13 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
 
         [HarmonyPatch(typeof(FishingRodWeapon))] static class FishingRodWeaponT
         {
-            static Vector3 Position = new Vector3(0, .25f, .3f);
-            static Vector3 Rotation = new Vector3(10, 90, 20);
-            static Vector3 Scale = new Vector3(.1f, .1f, .14f);
+            static Vector3 Position = new Vector3(0, .25f, .3f),
+                           Rotation = new Vector3(10, 90, 20),
+                           Scale = new Vector3(.1f, .1f, .14f);
 
             [HarmonyPostfix] [HarmonyPatch(nameof(FishingRodWeapon.Awake))] static void Retransform(FishingRodWeapon __instance)
             {
-                __instance.gameObject.GetComponent<WeaponPos>().defaultPos = Position;
-                __instance.gameObject.GetComponent<WeaponPos>().defaultRot = Rotation;
-                __instance.gameObject.GetComponent<WeaponPos>().defaultScale = Scale;
+                ApplyTransform(__instance.GetComponent<WeaponPos>(), Position, Rotation, Scale);
 
                 Arms.ArmController.WeaponArmCon WAC = __instance.gameObject.AddComponent<Arms.ArmController.WeaponArmCon>();
                 Arm A = Arm.FeedbackerPreset(__instance.transform);
@@ -121,6 +126,45 @@ namespace Plugin.VRTRAKILL.VRPlayer.Guns.Patches
             __instance.middlePos = __instance.defaultPos;
             __instance.middleRot = __instance.defaultRot;
             __instance.middleScale = __instance.defaultScale;
+        }
+
+        public static void ApplyTransform(ref WeaponPos WPos, Vector3 Position = new Vector3(), Vector3 EulerAngles = new Vector3(), Vector3 Scale = new Vector3())
+        {
+            if (WPos == null) return;
+            if (Position != new Vector3())
+            {
+                WPos.defaultPos = Position; WPos.middlePos = Position;
+                WPos.transform.localPosition = Position;
+            }
+            if (EulerAngles != new Vector3())
+            {
+                WPos.defaultRot = EulerAngles; WPos.middleRot = EulerAngles;
+                WPos.transform.localRotation = Quaternion.Euler(EulerAngles);
+            }
+            if (Scale != new Vector3())
+            {
+                WPos.defaultScale = Scale;
+                WPos.transform.localScale = Scale;
+            }
+        }
+        public static void ApplyTransform(WeaponPos WPos, Vector3 Position = new Vector3(), Vector3 EulerAngles = new Vector3(), Vector3 Scale = new Vector3())
+        {
+            if (WPos == null) return;
+            if (Position != new Vector3())
+            {
+                WPos.defaultPos = Position; WPos.middlePos = Position;
+                WPos.transform.localPosition = Position;
+            }
+            if (EulerAngles != new Vector3())
+            {
+                WPos.defaultRot = EulerAngles; WPos.middleRot = EulerAngles;
+                WPos.transform.localRotation = Quaternion.Euler(EulerAngles);
+            }
+            if (Scale != new Vector3())
+            {
+                WPos.defaultScale = Scale;
+                WPos.transform.localScale = Scale;
+            }
         }
     }
 }
