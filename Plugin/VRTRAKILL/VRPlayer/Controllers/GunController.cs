166cbcfc56ba1e4dd0e20ab48f47b383418d206f
whateverusername0 <whateveremail>
2023-10-07 15:55:08 +1000

    guncon: fixed sandboxarm not rendering

--- Plugin/VRTRAKILL/VRPlayer/Controllers/GunController.cs
+++ Plugin/VRTRAKILL/VRPlayer/Controllers/GunController.cs
@@ -19,7 +19,10 @@ namespace Plugin.VRTRAKILL.VRPlayer.Controllers
         }
         public void Start()
         {
-            RM = CC.RenderModel;
+            RM = CC.RenderModel.transform.GetChild(0).gameObject;
+            SandboxHandRM = CC.RenderModel.transform.GetChild(1).gameObject;
+            RM.SetActive(false);
+            SandboxHandRM.SetActive(false);
         }
 
         public void Update()
@@ -30,16 +33,27 @@ namespace Plugin.VRTRAKILL.VRPlayer.Controllers
 
         private void SetControllers()
         {
-            if (GunControl.Instance != null
-            && GunControl.Instance.currentWeapon != null
-            && GunControl.Instance.currentWeapon.HasComponent<Sandbox.Arm.SandboxArm>())
-                CC.RenderModel = SandboxHandRM;
-            else CC.RenderModel = RM;
-
-            if (!((bool)GunControl.Instance?.activated && (bool)GunControl.Instance?.noWeapons)
-            || ((bool)GunControl.Instance?.noWeapons && !Vars.IsPlayerFrozen))
-                CC.RenderModel.SetActive(true);
-            else CC.RenderModel.SetActive(false);
+            if ((bool)!GunControl.Instance?.activated || (bool)GunControl.Instance?.noWeapons)
+            {
+                if (GunControl.Instance != null
+                && GunControl.Instance.currentWeapon != null
+                && GunControl.Instance.currentWeapon.HasComponent<Sandbox.Arm.SandboxArm>()
+                && !Vars.IsMainMenu)
+                {
+                    RM.SetActive(false);
+                    SandboxHandRM.SetActive(true);
+                }
+                else
+                {
+                    RM.SetActive(true);
+                    SandboxHandRM.SetActive(false);
+                }
+            }
+            else
+            {
+                RM.SetActive(false);
+                SandboxHandRM.SetActive(false);
+            }
         }
     }
 }
