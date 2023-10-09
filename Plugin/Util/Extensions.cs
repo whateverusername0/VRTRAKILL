cdbd6dbf1a69d1d766f4e1325f0f403c4329e6be
whateverusername0 <whateveremail>
2023-10-09 19:54:54 +1000

    createa a separate class for extensions

---
+++ Plugin/Util/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Plugin.VRTRAKILL.Config;
+using System.IO;
+using UnityEngine; using Object = UnityEngine.Object;
+
+namespace Plugin.Util
+{
+    internal static class Extensions
+    {
+        public static bool HasComponent<T>(this GameObject GM) where T : Component
+        { return GM.GetComponent<T>() != null; }
+
+        /// <summary>
+        /// Converts a string to a Version
+        /// </summary>
+        /// <param name="sVersion"> Version as a string </param>
+        /// <returns></returns>
+        public static Version ToVersion(this string sVersion)
+        {
+            string[] Chars = sVersion.Split('.', '-', ' ');
+            return new Version(int.Parse(Chars[0]), int.Parse(Chars[1]), int.Parse(Chars[2]));
+        }
+
+        public static void ChangeWrite(this NewConfig Config, string SettingName, object Value)
+        {
+            Config.GetType().GetProperty(SettingName).SetValue(Config, Value);
+            File.WriteAllText(ConfigMaster.ConfigPath, JsonConvert.SerializeObject(Config, Formatting.Indented));
+        }
+    }
+}
