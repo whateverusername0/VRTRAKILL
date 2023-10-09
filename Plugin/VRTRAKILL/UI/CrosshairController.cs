5df6dd213f79a439f6da54c65cce0e292419c65d
whateverusername0 <whateveremail>
2023-10-09 19:59:38 +1000

    crosshaircontroller: improve documentation, hopefully fixed weapons not aligning correctly with the crosshair

--- Plugin/VRTRAKILL/UI/CrosshairController.cs
+++ Plugin/VRTRAKILL/UI/CrosshairController.cs
@@ -3,21 +3,25 @@ using Plugin.Util;
 
 namespace Plugin.VRTRAKILL.UI
 {
+    /// <summary> Controls the behavior of player's crosshair. </summary>
     internal class CrosshairController : MonoBehaviour
     {
+        // Distance between the object and the crosshair
         public float Length => Vars.Config.CBS.CrosshairDistance;
 
+        // A magic number which gives us the most accurate center
         Vector3 Offset = new Vector3(-.2f, -2.75f, 0);
-        Transform Target;
+        Transform Target; // Where crosshair will point from
 
         public void LateUpdate()
         {
+            // Doesn't work. (for some reason)
             if ((Vars.IsPlayerFrozen || Vars.IsPlayerUsingShop) && !Vars.Config.UIInteraction.ControllerBased)
                 transform.position = Vars.MainCamera.transform.position + (Vars.MainCamera.transform.forward * Length * .25f) + Offset;
             else
             {
-                if (GunControl.Instance != null && GunControl.Instance.currentWeapon.HasComponent<RocketLauncher>())
-                    Target = GunControl.Instance.currentWeapon.transform;
+                // This otoh works.
+                if (GunControl.Instance != null) Target = GunControl.Instance.currentWeapon.transform;
                 else Target = Vars.DominantHand.transform;
 
                 transform.position = Target.position + (Target.forward * Length) + Offset;
