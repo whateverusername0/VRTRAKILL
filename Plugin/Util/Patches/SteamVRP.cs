a234afad53a699a194f56d24bccaa89a5db8da43
whateverusername0 <whateveremail>
2023-10-09 19:54:26 +1000

    steamvrp.cs: update documentation

--- Plugin/Util/Patches/SteamVRP.cs
+++ Plugin/Util/Patches/SteamVRP.cs
@@ -6,10 +6,11 @@ namespace Plugin.Util.Patches
 {
     [HarmonyPatch] internal static class SteamVRP
     {
+        // Since SteamVR_Camera is a tasty piece and I don't want to reinvent the wheel
+        // by fucking around w/ namings and break the game halfway through, i made this.
         [HarmonyPrefix] [HarmonyPatch(typeof(SteamVR_Camera), nameof(SteamVR_Camera.Expand))]
         static bool SVRCExpand(SteamVR_Camera __instance, Transform ____ears, Transform ____head)
         {
-            // I can't believe i'm patching a whole ass svr method to fit my needs. oh well
             __instance.gameObject.AddComponent<SteamVR_Ears>();
             ____ears = __instance.transform;
 
