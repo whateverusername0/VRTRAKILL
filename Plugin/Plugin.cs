0da4b45e2a052f8f2586967055fbda2e96287f5b
whateverusername0 <whateveremail>
2023-10-09 20:01:36 +1000

    create plugininfocs where plugin stuff will now be stored. plugin.cs: documentation & codebase improvements

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -1,9 +1,5 @@
-﻿using System.IO;
-using System.Reflection;
-using System.Diagnostics;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
-using BepInEx.Bootstrap;
 using Valve.VR;
 using Plugin.Util;
 using Plugin.VRTRAKILL;
@@ -15,22 +11,13 @@ namespace Plugin
      * This is a dumpster fire of spaghetti, inconsistent
      * naming, questionable life choices and shitty performance. Amen. */
 
-    // Dependencies (or other mods support)
+    // Dependencies are for compatibility with other mods.
     [BepInDependency("com.eternalUnion.pluginConfigurator", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("xzxADIxzx.Jaket", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)] public sealed class Plugin : BaseUnityPlugin
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public sealed partial class Plugin : BaseUnityPlugin
     {
         internal static ManualLogSource PLog { get; private set; }
-
-        public const string
-            PLUGIN_GUID = "com.whateverusername0.vrtrakill",
-            PLUGIN_NAME = "VRTRAKILL",
-            PLUGIN_VERSION = "0.15.2";
-
-        public static string
-            PluginPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-            FullGamePath = Process.GetCurrentProcess().MainModule.FileName,
-            GamePath = Path.GetDirectoryName(FullGamePath);
 
         public void Awake()
         {
@@ -44,8 +31,9 @@ namespace Plugin
 
             InitializeSteamVR();
 
-            CheckForModSupport();
+            VRTRAKILL.Compatibility.Initializer.Start();
         }
+
         private void PatchStuff()
         {
             System.Collections.Generic.List<string> Namespaces = new System.Collections.Generic.List<string>
@@ -63,38 +51,25 @@ namespace Plugin
                 typeof(VRTRAKILL.VRPlayer.Controllers.Patches.ControllerAdder),
                 typeof(VRTRAKILL.Input.ControlMessages.Patches)
             };
-            if (Vars.Config.Controllers.EnableHaptics)
-                Types.Add(typeof(VRTRAKILL.VRPlayer.Controllers.Patches.ControllerHaptics));
-            if (Vars.Config.EnableCBS)
-                Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Guns.Patches.A).Namespace);
-            if (Vars.Config.EnableMBP)
-                Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Arms.Patches.A).Namespace);
-            if (!Vars.Config.MBP.CameraWhiplash)
-                Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Arms.Patches.Whiplash.A).Namespace);
-            if (Vars.Config.EnableVRBody)
-                Namespaces.Add(typeof(VRTRAKILL.VRPlayer.VRAvatar.Patches.A).Namespace);
+            if (Vars.Config.Controllers.EnableHaptics) Types.Add(typeof(VRTRAKILL.VRPlayer.Controllers.Patches.ControllerHaptics));
+            if (Vars.Config.EnableCBS)                 Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Guns.Patches.A).Namespace);
+            if (Vars.Config.EnableMBP)                 Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Arms.Patches.A).Namespace);
+            if (!Vars.Config.MBP.CameraWhiplash)       Namespaces.Add(typeof(VRTRAKILL.VRPlayer.Arms.Patches.Whiplash.A).Namespace);
+            if (Vars.Config.EnableVRBody)              Namespaces.Add(typeof(VRTRAKILL.VRPlayer.VRAvatar.Patches.A).Namespace);
 
-            new Patcher(new HarmonyLib.Harmony($"{PLUGIN_GUID}.base"))
+            new Patcher(new HarmonyLib.Harmony($"{PluginInfo.PLUGIN_GUID}.base"))
             {
                 Namespaces = Namespaces.ToArray(),
                 Types = Types.ToArray()
             }.PatchAll();
         }
+
         private void InitializeSteamVR()
         {
             SteamVR_Actions.PreInitialize();
             SteamVR.Initialize();
 
             VRTRAKILL.Input.SVRActionsManager.Init();
-        }
-
-        private void CheckForModSupport()
-        {
-            foreach (var Plugin in Chainloader.PluginInfos)
-            {
-                VRTRAKILL.ModSupport.Initializers.Mods.TryGetValue(Plugin.Value.Metadata.GUID, out System.Action<object> A);
-                A?.Invoke(null);
-            }
         }
     }
 }
