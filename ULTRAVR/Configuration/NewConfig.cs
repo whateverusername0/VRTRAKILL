f57a10b4ae22d142630e3ddd626ce88f2e086830
whateverusername0 <whateveremail>
2025-03-03 17:06:20 +1000

    stuff

---
+++ ULTRAVR/Configuration/NewConfig.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+
+#pragma warning disable IDE1006 // Naming Styles
+namespace ULTRAVR.Configuration
+{
+    public class NewConfig
+    {
+        // since ultrakill is running on basic Update and headset refresh rates are different we gotta do sth about it.
+        [JsonProperty("Movement multiplier")] public float MovementMultiplier { get; set; } = 0.575f;
+
+        [JsonIgnore] public UKBindings.ModifiedActions UKBinds { get; set; }
+        [JsonProperty("VRTRAKILL Keybinds")] public _VRBinds VRBinds { get; set; } public class _VRBinds
+        {
+            [JsonProperty("Toggle Desktop View")] public string ToggleDV { get; set; } = "T";
+        }
+
+        [JsonProperty("Controllers")] public _Controllers Controllers { get; set; } public class _Controllers
+        {
+            [JsonProperty("Deadzone (from 0 to 1)")] public float Deadzone { get; set; } = 0.4f;
+            [JsonProperty("Smooth turning speed")] public float SmoothSpeed { get; set; } = 300;
+            [JsonProperty("Snap turning")] public bool SnapTurn { get; set; } = false;
+            [JsonProperty("Snap turning angles")] public float SnapAngles { get; set; } = 45;
+
+            [JsonProperty("Draw controller models")] public bool DrawControllers { get; set; } = true;
+            [JsonProperty("Enable Haptics")] public bool EnableHaptics { get; set; } = true;
+        }
+
+        [JsonProperty("Gameplay")] public _Gameplay CBS { get; set; } public class _Gameplay
+        {
+            [JsonProperty("Crosshair distance")] public float CrosshairDistance { get; set; } = 8;
+            [JsonProperty("Required speed to punch")] public float PunchingSpeed { get; set; } = 7.5f;
+        }
+
+        [JsonProperty("Accessibility")] public _Accessibility Acc { get; set; } public class _Accessibility
+        {
+            [JsonProperty("WHIPLASH: camera-based aiming")] public bool CameraWhiplash { get; set; } = false;
+        }
+
+        [JsonProperty("In-game body")] public _Avatar Avatar { get; set; } public class _Avatar
+        {
+            [JsonProperty("Enabled")] public bool Enabled { get; set; } = false;
+            [JsonProperty("IK Arms")] public bool Arms { get; set; } = true;
+            [JsonProperty("IK Legs")] public bool Legs { get; set; } = true;
+        }
+
+        [JsonProperty("UI")] public _UIInteraction UIInteraction { get; set; } public class _UIInteraction
+        {
+            [JsonProperty("UI Size (from 0 to 0.1)")] public float UISize { get; set; } = 0.0625f;
+
+            [JsonProperty("Controller Lines")] public _ControllerLines ControllerLines { get; set; } public class _ControllerLines
+            {
+                [JsonProperty("Enabled")] public bool Enabled { get; set; } = true;
+                [JsonProperty("Start alpha (from 0 to 1)")] public float StartAlpha { get; set; } = 0.4f;
+                [JsonProperty("End alpha (from 0 to 1)")] public float EndAlpha { get; set; } = 0.1f;
+            }
+
+            public _UIInteraction()
+            {
+                ControllerLines = new _ControllerLines();
+            }
+        }
+
+        [JsonProperty("DesktopView Settings")] public _DesktopView DesktopView { get; set; } public class _DesktopView
+        {
+            [JsonProperty("Enabled by default")] public bool Enabled { get; set; } = true;
+            [JsonProperty("World view FOV")] public float WorldCamFOV { get; set; } = 90;
+            [JsonProperty("UI view FOV")] public float UICamFOV { get; set; } = 90;
+        }
+
+        public NewConfig()
+        {
+            UKBinds = UKBindings.GetBinds();
+            VRBinds = new _VRBinds();
+        }
+    }
+}
+#pragma warning restore IDE1006 // Naming Styles
---
+++ ULTRAVR/Versioning/Version.cs
@@ -0,0 +1,66 @@
+﻿namespace Versioning
+{
+    /// <summary>
+    /// Reprenents a version of any kind.
+    /// Structure: <c>"Major.Minor.Patch"</c>
+    /// </summary>
+    public readonly struct Version
+    {
+        private readonly int? _Major, _Minor, _Patch;
+        public int Major => _Major ?? 0;
+        public int Minor => _Minor ?? 0;
+        public int Patch => _Patch ?? 0;
+
+        public Version(int Major) : this()
+        => _Major = Major;
+        public Version(int Major, int Minor) : this(Major)
+        => _Minor = Minor;
+        public Version(int Major, int Minor, int Patch) : this(Major, Minor)
+        => _Patch = Patch;
+
+        public static bool operator ==(Version V1, Version V2)
+        {
+            if (V1.Major == V2.Major && V1.Minor == V2.Minor && V1.Patch == V2.Patch) return true;
+            else return false;
+        }
+        public static bool operator !=(Version V1, Version V2) => !(V1 == V2);
+
+        public static bool operator >(Version V1, Version V2)
+        {
+            if ((V1.Major > V2.Major)
+            || (V1.Major == V2.Major && V1.Minor > V2.Minor)
+            || (V1.Major == V2.Major && V1.Minor == V2.Minor && V1.Patch > V2.Patch)) return true;
+            else return false;
+        }
+        public static bool operator <(Version V1, Version V2)
+        {
+            if ((V1.Major < V2.Major)
+            || (V1.Major == V2.Major && V1.Minor < V2.Minor)
+            || (V1.Major == V2.Major && V1.Minor == V2.Minor && V1.Patch < V2.Patch)) return true;
+            else return false;
+        }
+
+        public override bool Equals(object obj) => base.Equals(obj);
+        public override int GetHashCode()
+        {
+            string S = ToString();
+            return S.GetHashCode();
+        }
+
+        public override string ToString() => $"{Major}.{Minor}.{Patch}";
+    }
+
+    public static class VersionExtensions
+    {
+        /// <summary>
+        /// Converts a string to a Version
+        /// </summary>
+        /// <param name="sVersion"> Version as a string </param>
+        /// <returns></returns>
+        public static Version ToVersion(this string sVersion)
+        {
+            string[] Chars = sVersion.Split('.', '-', ' ');
+            return new Version(int.Parse(Chars[0]), int.Parse(Chars[1]), int.Parse(Chars[2]));
+        }
+    }
+}
